98753ce1a6e72153fe06d1e0cb74723a0b6094fb
luclegen <luc.dev.sun@gmail.com>
2020-05-11 11:43:52 +0700

    Database Configuration

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,3 +4,5 @@ mongoose.connect(process.env.MONGODB_URI, err => {
   if (err) console.log('ERROR: MongoDB connection is failed: ' + JSON.stringify(err, undefined, 2));
   else console.log('MongoDB connection succeeded.');
 });
+
+require('../models/user.model');
