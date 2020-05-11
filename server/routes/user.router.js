4213b9310d1755811ce71274f2f4d19464f2e8b3
luclegen <luc.dev.sun@gmail.com>
2020-05-11 17:28:22 +0700

    User Router: GET profile

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const userController = require('../controllers/user.controller');
 
+const jwt = require('../helpers/jwt');
+
 router.post('/register', userController.register);
 router.post('/verify-email/:id', userController.verifyEmail);
 router.get('/resend-verify-email/:id', userController.resendVerifyEmail);
@@ -12,5 +14,6 @@ router.post('/find-username', userController.findUsername);
 router.get('/resend-verify-reset-password/:username', userController.resendVerifyResetPassword);
 router.put('/reset-password/:username', userController.resetPassword);
 router.put('/change-password/:id', userController.changePassword);
+router.get('/profile', jwt.verifyJwtToken, userController.profile);
 
 module.exports = router;
