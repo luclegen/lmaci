4e15be0e9b5daaaa36535c3e66851f3ce18da4de
luclegen <luc.dev.sun@gmail.com>
2020-05-11 15:40:08 +0700

    Helpers: Mailer

--- server/helpers/mailer.js
+++ server/helpers/mailer.js
@@ -11,22 +11,20 @@ const transport = nodemailer.createTransport({
   }
 });
 
-module.exports = {
-  sendVerifyEmail(clientEmail, title, code) {
-    let mailOptions = {
-      from: {
-        "address": process.env.SERVER_GMAIL,
-        "name": process.env.SERVER_NAME
-      },
-      to: clientEmail,
-      subject: title,
-      text: '',
-      html: '<h1><img src="https://raw.githubusercontent.com/luclegen/lmaci/master/client/src/assets/img/logo/lmaci-logo-64.png" alt="Lmaci Logo"> Lmaci</h1><hr><h1>' + title + '</h1><p>Verification Code: ' + code + '</p>'
-    }
-
-    transport.sendMail(mailOptions, (err, info) => {
-      if (err) console.log(err);
-      else console.log('Email sent: ' + info.response);
-    });
+module.exports.sendVerifyEmail = (clientEmail, title, code) => {
+  let mailOptions = {
+    from: {
+      "address": process.env.SERVER_GMAIL,
+      "name": process.env.SERVER_NAME
+    },
+    to: clientEmail,
+    subject: title,
+    text: '',
+    html: '<h1><img src="https://raw.githubusercontent.com/luclegen/lmaci/master/client/src/assets/img/logo/lmaci-logo-64.png" alt="Lmaci Logo"> Lmaci</h1><hr><h1>' + title + '</h1><p>Verification Code: ' + code + '</p>'
   }
+
+  transport.sendMail(mailOptions, (err, info) => {
+    if (err) console.log(err);
+    else console.log('Email sent: ' + info.response);
+  });
 }
