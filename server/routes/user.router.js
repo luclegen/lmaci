a0332b35516876a6ffa8636bacb7c558eb5c093d
luclegen <luc.dev.sun@gmail.com>
2020-05-11 17:10:37 +0700

    User Router: POST findUsername

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -8,5 +8,6 @@ router.post('/verify-email/:id', userController.verifyEmail);
 router.get('/resend-verify-email/:id', userController.resendVerifyEmail);
 router.put('/change-email/:id', userController.changeEmail);
 router.post('/authenticate', userController.authenticate);
+router.post('/find-username', userController.findUsername);
 
 module.exports = router;
