4b8772c8296f4f91f10d0409586fa92e34eeef90
luclegen <luc.dev.sun@gmail.com>
2020-05-11 15:46:15 +0700

    Helpers: Name Converter

--- server/helpers/name-converter.js
+++ server/helpers/name-converter.js
@@ -1,3 +1,3 @@
-module.exports.convertName = (phrase) => {
+module.exports.convertName = phrase => {
   return phrase.trim().split(/\s+/).map(w => w[0].toUpperCase() + w.slice(1).toLowerCase()).join(' ');
 }
