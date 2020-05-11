ef852ebc1605c1a04c21980fc22e755c3cc83de8
luclegen <luc.dev.sun@gmail.com>
2020-05-11 15:45:53 +0700

    Helpers: Code Generator

--- server/helpers/code-generator.js
+++ server/helpers/code-generator.js
@@ -1,4 +1,4 @@
-module.exports.generateCode = (length) => {
+module.exports.generateCode = length => {
   let code = '', characters = '0123456789';
   for (let i = 0; i < length; i++) code += characters.charAt(Math.floor(Math.random() * characters.length));
   return code;
