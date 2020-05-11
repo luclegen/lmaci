9ceefc289a54d9bb3c92af6495edc4e893d4156b
luclegen <luc.dev.sun@gmail.com>
2020-05-11 15:07:07 +0700

    User Router: POST verifyEmail

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 
 router.post('/register', userController.register);
+router.post('/verify-email/:id', userController.verifyEmail);
 
 module.exports = router;
