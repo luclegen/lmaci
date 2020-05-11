7e643c371e943eceebdd04cdfc9dc6cee1ec04f4
luclegen <luc.dev.sun@gmail.com>
2020-05-11 10:57:03 +0700

    Command

---
+++ cmd.sh
@@ -0,0 +1,22 @@
+# COMMAND
+
+# * Prerequisites *
+sudo npm install -g @angular/cli
+sudo npm install -g nodemon
+
+# I. Angular Client
+ng new client
+cd client; ng s -o
+ng g class user --type=model
+
+# Optional
+kill -9 $(lsof -t -i:4200); clear; ng s
+
+# II. Nodejs Server
+
+mkdir server; cd server; npm init
+npm i --save express mongoose body-parser bcryptjs cors jsonwebtoken passport passport-local lodash nodemailer
+clear; nodemon
+
+# Optional
+kill -9 $(lsof -t -i:3000); clear; nodemon
