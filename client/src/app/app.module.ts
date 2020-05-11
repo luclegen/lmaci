f219fb6b978f068252607d9c84cb9eb0dc0ab210
luclegen <luc.dev.sun@gmail.com>
2020-05-11 17:35:30 +0700

    Body Component: Initial

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { BodyComponent } from './body/body.component';
+import { BodyComponent } from './components/body/body.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
