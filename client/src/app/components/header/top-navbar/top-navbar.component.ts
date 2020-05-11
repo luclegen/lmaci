1f357db6030356b8ef7359c0d25fae74cfbd1920
luclegen <luc.dev.sun@gmail.com>
2020-05-11 19:10:36 +0700

    Top Navbar Component: expandA

--- client/src/app/components/header/top-navbar/top-navbar.component.ts
+++ client/src/app/components/header/top-navbar/top-navbar.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNavbarComponent implements OnInit {
 
+  userDetails;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  expandA(id, master, nav, height) {
+    let a_id = document.getElementById(id),
+        master_style = document.getElementById(master).style,
+        nav_style = document.getElementById(nav).style,
+        a = document.getElementsByClassName('a') as HTMLCollectionOf<HTMLElement>,
+        li = document.getElementsByClassName('li') as HTMLCollectionOf<HTMLElement>,
+        sub_nav = document.getElementsByClassName('sub-nav') as HTMLCollectionOf<HTMLElement>;
+    
+    // Close all a tag
+    if (a_id.getAttribute('aria-expanded') == 'false') {
+      for (let i = 0; i < a.length; i++) {
+        if (a[i].getAttribute('aria-expanded') == 'true') {
+          li[i].style.backgroundColor = 'lime';
+          sub_nav[i].style.height = '0';
+          a[i].setAttribute('aria-expanded', 'false');
+        }
+      }
+      nav_style.height = height;
+      master_style.backgroundColor = 'green';
+      a_id.setAttribute('aria-expanded', 'true');
+    } else {
+      nav_style.height = '0';
+      master_style.backgroundColor = 'lime';
+      a_id.setAttribute('aria-expanded', 'false');
+    }
+  }
 }
