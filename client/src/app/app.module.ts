169636b5286c8a682f7d4eb2891f3b3d41e81358
luclegen <luc.dev.sun@gmail.com>
2020-05-11 17:47:43 +0700

    Home Component: Initial

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { TopNavbarComponent } from './components/header/top-navbar/top-navbar.component';
+import { HomeComponent } from './components/body/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    TopNavbarComponent
+    TopNavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
