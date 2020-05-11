6aeaecf92c1da93a6083223e03460d21acf7b587
luclegen <luc.dev.sun@gmail.com>
2020-05-11 12:01:16 +0700

    Server App

--- server/app.js
+++ server/app.js
@@ -1,3 +1,29 @@
+// Configuration
 require('./configs/config');
+require('./configs/db');
+require('./configs/passport');
 
+// Initialization
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
 
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use(passport.initialize());
+
+// Error handle
+app.use((err, req, res, next) => {
+  if (err.name === 'ValidationError') {
+    let valErrors = [];
+    Object.keys(err.errors).forEach(key => valErrors.push(err.errors[key].message));
+    res.status(442).send(valErrors);
+  }
+});
+
+// Start Server
+app.listen(process.env.PORT, () => console.log(`Server started at port: ${process.env.PORT}`));
