7bbada7a4d0a6a4278d06c12aab35255af2893a9
luclegen <luc.dev.sun@gmail.com>
2020-05-12 10:31:37 +0700

    Routes: UserComponent, RegisterComponent

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './components/body/home/home.component';
+import { UserComponent } from './components/body/user/user.component';
+import { RegisterComponent } from './components/body/user/register/register.component';
 
 export const routes: Routes = [
+  {
+    path: 'user', component: UserComponent,
+    children: [ { path: 'register', component: RegisterComponent } ]
+  },
   {
     path: '', pathMatch: 'full', component: HomeComponent
   }
