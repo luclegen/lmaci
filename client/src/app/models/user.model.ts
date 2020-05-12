aef890dbd1c667777ef554ed04b2a6e4e4809631
luclegen <luc.dev.sun@gmail.com>
2020-05-12 09:56:00 +0700

    Models: user

---
+++ client/src/app/models/user.model.ts
@@ -0,0 +1,14 @@
+export class User {
+  avatar: string;
+  firstName: string;
+  lastName: string;
+  fullName: string;
+  role: string;
+  email: string;
+  emailVerifyCode: string;
+  emailVerified: boolean;
+  mobileNumber: string;
+  username: string;
+  password: string;
+  address: string;
+}
