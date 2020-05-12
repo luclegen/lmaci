49c751bdb249a111856cec72af1e2da1e89dcade
luclegen <luc.dev.sun@gmail.com>
2020-05-12 11:16:45 +0700

    Login Component: Fields

--- client/src/app/components/body/user/login/login.component.ts
+++ client/src/app/components/body/user/login/login.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  title = 'Login | Lmaci';
+  user = {
+    username: '',
+    password: ''
+  };
+  usernameRegex;
+  serverErrorMessages: string;
+
   constructor() { }
 
   ngOnInit(): void {
