ddf3caa3573e9e9f4ace4c9f36583d93c560fcd3
luclegen <luc.dev.sun@gmail.com>
2020-05-12 11:25:39 +0700

    Routes: LoginComponent

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/body/home/home.component';
 import { UserComponent } from './components/body/user/user.component';
 import { RegisterComponent } from './components/body/user/register/register.component';
+import { LoginComponent } from './components/body/user/login/login.component';
 
 export const routes: Routes = [
   {
     path: 'user', component: UserComponent,
-    children: [ { path: 'register', component: RegisterComponent } ]
+    children: [ { path: 'register', component: RegisterComponent },
+                { path: 'login', component: LoginComponent }]
   },
   {
     path: '', pathMatch: 'full', component: HomeComponent
