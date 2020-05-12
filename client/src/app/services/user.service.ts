53a8ec99ff655024cbe7859bdb516afc27d959c7
luclegen <luc.dev.sun@gmail.com>
2020-05-12 11:06:53 +0700

    User Service: usernameRegex

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -25,7 +25,7 @@ export class UserService {
   
   noAuthHeader = { headers: new HttpHeaders({ 'noAuth': 'True' }) };
   
-  usernameRegex = /^(?=[a-zA-Z0-9._]{1,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+  usernameRegex = /^(?=[a-zA-Z0-9._]{1,20}$)/;
   codeRegex = /^\d{6}$/;
   emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   mobileNumberRegex = /^(\+\d{1,3}[- ]?)?\d{10}$/;
