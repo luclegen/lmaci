ef8b758800b263f5cd2a2f631fec78fa191aca55
luclegen <luc.dev.sun@gmail.com>
2020-05-12 10:05:08 +0700

    Services: Initial

---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  selectedUser: User = {
+    avatar: '',
+    firstName: '',
+    lastName: '',
+    fullName: '',
+    role: '',
+    email: '',
+    emailVerifyCode: '',
+    emailVerified: false,
+    mobileNumber: '',
+    username: '',
+    password: '',
+    address: ''
+  };
+  
+  noAuthHeader = { headers: new HttpHeaders({ 'noAuth': 'True' }) };
+  
+  usernameRegex = /^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+  codeRegex = /^\d{6}$/;
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  mobileNumberRegex = /^(\+\d{1,3}[- ]?)?\d{10}$/;
+  
+  constructor(private http: HttpClient) { }
+}
