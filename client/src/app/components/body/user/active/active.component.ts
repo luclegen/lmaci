6ec8f1f8d7118ee5d9ef192639b1d555939282ba
luclegen <luc.dev.sun@gmail.com>
2020-05-13 16:11:58 +0700

    Actice Component: constructor

--- client/src/app/components/body/user/active/active.component.ts
+++ client/src/app/components/body/user/active/active.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-active',
@@ -14,7 +18,10 @@ export class ActiveComponent implements OnInit {
 
   serverErrorMessages: string;
 
-  constructor() { }
+  constructor(private titleService: Title, private userService: UserService, private router: Router) {
+    this.titleService.setTitle('Verify Email | Lmaci');
+    this.codeRegex = this.userService.codeRegex;
+  }
 
   ngOnInit(): void {
   }
