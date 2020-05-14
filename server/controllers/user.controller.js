b8a7880742f8e4550fa4a0703666a305fa796326
luclegen <luc.dev.sun@gmail.com>
2020-05-14 17:05:17 +0700

    Auth Controller: info

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,9 +1,8 @@
 const User = require('../models/user.model');
 
 module.exports.get = (req, res) => {
-  console.log(req.params.username);
   User.findOne({ username: req.params.username }, (err, user) => {
-    return user ? res.status(200).json(user)
+    return user ? res.status(200).json({ user })
                 : res.status(404).json({ msg: 'User not found.' });
   });
 }
