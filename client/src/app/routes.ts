880515bc41d57be9040e6c2204359d2e8e651ac9
luclegen <luc.dev.sun@gmail.com>
2020-05-14 11:42:54 +0700

    Routes: AuthComponent

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -4,18 +4,22 @@ import { AuthGuard } from './auth/auth.guard';
 
 import { HomeComponent } from './components/body/home/home.component';
 import { UserComponent } from './components/body/user/user.component';
+import { ProfileComponent } from './components/body/user/profile/profile.component';
+import { AuthComponent } from './components/body/auth/auth.component';
 import { RegisterComponent } from './components/body/auth/register/register.component';
 import { LoginComponent } from './components/body/auth/login/login.component';
 import { ActiveComponent } from './components/body/auth/active/active.component';
-import { ProfileComponent } from './components/body/user/profile/profile.component';
 
 export const routes: Routes = [
   {
-    path: 'user', component: UserComponent,
+    path: '', component: AuthComponent,
     children: [ { path: 'register', component: RegisterComponent },
                 { path: 'login', component: LoginComponent },
-                { path: 'active', component: ActiveComponent, canActivate: [AuthGuard] },
-                { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] } ]
+                { path: 'active', component: ActiveComponent, canActivate: [AuthGuard] } ]
+  },
+  {
+    path: 'user', component: UserComponent,
+    children: [ { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] } ]
   },
   {
     path: '', pathMatch: 'full', component: HomeComponent
