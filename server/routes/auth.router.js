65ed566c531990a5487594b356f067aff64bdfe8
luclegen <luc.dev.sun@gmail.com>
2020-05-14 11:04:57 +0700

    Auth Router

--- server/routes/auth.router.js
+++ server/routes/auth.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const authController = require('../controllers/user.controller');
+const authController = require('../controllers/auth.controller');
 
 const jwt = require('../helpers/jwt');
 
