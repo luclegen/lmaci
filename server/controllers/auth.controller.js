7878675d40e8ae85833531645b9ff08a5dbac17a
luclegen <luc.dev.sun@gmail.com>
2020-05-14 16:13:23 +0700

    Auth Controller: info

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -203,9 +203,9 @@ module.exports.changePassword = (req, res) => {
   });
 }
 
-module.exports.profile = (req, res) => {
+module.exports.info = (req, res) => {
   User.findById(req._id, (err, user) => {
-    return user ? res.status(200).json({ status: true, user: _.pick(user, [ 'avatar', 'firstName', 'fullName', 'gender', 'email', 'activated', 'mobileNumber', 'username', 'role', 'address']) })
+    return user ? res.status(200).json({ status: true, user: _.pick(user, [ 'avatar', 'firstName', 'activated', 'username', 'role']) })
                 : res.status(404).json({ status: false, msg: 'User not found.' });
   });
 }
