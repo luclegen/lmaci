3288a01491f119fcf9dfa5d3a1e5a317f0a8a09e
luclegen <luc.dev.sun@gmail.com>
2020-05-14 11:47:12 +0700

    Auth Service: Initial

--- client/src/app/auth/auth.guard.ts
+++ client/src/app/auth/auth.guard.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 
-import { UserService } from '../services/user.service';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
-      if (!this.userService.isLoggedIn()) {
+      if (!this.authService.isLoggedIn()) {
         this.router.navigateByUrl('');
-        this.userService.removeToken();
+        this.authService.removeToken();
         return false
       }
     return true;
--- client/src/app/auth/auth.interceptor.ts
+++ client/src/app/auth/auth.interceptor.ts
@@ -6,19 +6,19 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { UserService } from '../services/user.service';
 import { tap } from "rxjs/operators";
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   intercept(req: HttpRequest<unknown>, next: HttpHandler) {
     if (req.headers.get('noauth')) return next.handle(req.clone());
     else {
       const clonereq = req.clone({
-        headers: req.headers.set('Authorization', 'Bearer ' + this.userService.getToken())
+        headers: req.headers.set('Authorization', 'Bearer ' + this.authService.getToken())
       });
       return next.handle(clonereq).pipe(
         tap (
