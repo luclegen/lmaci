f402ebe4f9961a0aa640b17991a15950783d3efc
luclegen <luc.dev.sun@gmail.com>
2020-05-14 18:02:55 +0700

    Profile Component: ngOnInit

--- client/src/app/components/body/user/profile/profile.component.ts
+++ client/src/app/components/body/user/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { UserService } from 'src/app/services/user.service';
 import { AuthService } from 'src/app/services/auth.service';
@@ -14,24 +14,32 @@ export class ProfileComponent implements OnInit {
   userDetails;
   title = '\'s Profile';
   
-  constructor(private titleService: Title, route: ActivatedRoute, private authService: AuthService, private userService: UserService) { }
+  constructor(private titleService: Title,
+              private route: ActivatedRoute, 
+              private authService: AuthService, 
+              private userService: UserService, 
+              private router: Router) { }
 
   ngOnInit(): void {
+    const username = this.route.snapshot.paramMap.get('username');
     this.authService.getInfo().subscribe(
       res => {
-
-      },
-
+        if(res['user'].role === 'root' || res['user'].role === 'admin' || res['user'].username === username) {
+          this.userService.getUser(username).subscribe(
+            res => {
+              this.userDetails = res['user'];
+              this.titleService.setTitle(this.userDetails.firstName + this.title);
+              this.userDetails.role = this.userDetails.role.split('')[0].toUpperCase() + this.userDetails.role.split('').slice(1).join('');
+              this.userDetails.gender = this.userDetails.gender.split('')[0].toUpperCase() + this.userDetails.gender.split('').slice(1).join('');
+            },
+            err => {
+              alert(err.error.msg);
+              this.router.navigateByUrl('');
+            }
+          );
+        } else this.router.navigateByUrl('');
+      }
     );
-    // this.userService.getProfile().subscribe(
-    //   res => {
-    //     this.userDetails = res['user'];
-    //     this.titleService.setTitle(this.userDetails.firstName + this.title);
-    //     this.userDetails.role = this.userDetails.role.split('')[0].toUpperCase() + this.userDetails.role.split('').slice(1).join('');
-    //     this.userDetails.gender = this.userDetails.gender.split('')[0].toUpperCase() + this.userDetails.gender.split('').slice(1).join('');
-    //   },
-    //   err => {}
-    // );
   }
 
 }
