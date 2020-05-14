f56cfb114b80853fce8761e3fb4a3579f7e9cac9
luclegen <luc.dev.sun@gmail.com>
2020-05-14 16:32:27 +0700

    User Controller: get

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,10 +1,9 @@
-const _ = require('lodash');
-
 const User = require('../models/user.model');
 
-module.exports.profile = (req, res) => {
-  User.findById(req._id, (err, user) => {
-    return user ? res.status(200).json({ status: true, user: _.pick(user, [ 'avatar', 'firstName', 'fullName', 'gender', 'email', 'activated', 'mobileNumber', 'username', 'role', 'address']) })
-                : res.status(404).json({ status: false, msg: 'User not found.' });
+module.exports.get = (req, res) => {
+  console.log(req.params.username);
+  User.findOne({ username: req.params.username }, (err, user) => {
+    return user ? res.status(200).json(user)
+                : res.status(404).json({ msg: 'User not found.' });
   });
 }
