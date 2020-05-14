05ae56726fb26bf8403568db600b61a854e22551
luclegen <luc.dev.sun@gmail.com>
2020-05-14 09:38:36 +0700

    Routes: ProfileComponent

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -7,13 +7,15 @@ import { UserComponent } from './components/body/user/user.component';
 import { RegisterComponent } from './components/body/user/register/register.component';
 import { LoginComponent } from './components/body/user/login/login.component';
 import { ActiveComponent } from './components/body/user/active/active.component';
+import { ProfileComponent } from './components/body/user/profile/profile.component';
 
 export const routes: Routes = [
   {
     path: 'user', component: UserComponent,
     children: [ { path: 'register', component: RegisterComponent },
                 { path: 'login', component: LoginComponent },
-                { path: 'active', component: ActiveComponent, canActivate: [AuthGuard] } ]
+                { path: 'active', component: ActiveComponent, canActivate: [AuthGuard] },
+                { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] } ]
   },
   {
     path: '', pathMatch: 'full', component: HomeComponent
