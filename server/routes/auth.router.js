d3cee66989141f712d6b3cf15cc4b6059279898d
luclegen <luc.dev.sun@gmail.com>
2020-05-14 16:23:31 +0700

    Auth Router: GET info

--- server/routes/auth.router.js
+++ server/routes/auth.router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../controllers/auth.controller');
+const jwt = require('../helpers/jwt');
 
 router.post('/register', authController.register);
 router.post('/active/:id', authController.active);
