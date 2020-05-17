d96e1d025c73a454afcfcd1aa96c9c630f0257d6
luclegen <luc.dev.sun@gmail.com>
2020-05-17 16:56:12 +0700

    Reset Password Component: checkStrengthPassword

--- client/src/app/components/body/auth/reset-password/reset-password.component.ts
+++ client/src/app/components/body/auth/reset-password/reset-password.component.ts
@@ -24,7 +24,24 @@ export class ResetPasswordComponent implements OnInit {
   }
 
   checkStrengthPassword() {
+    let count = 0
+    const strength = {
+      0: "Worst",
+      1: "Bad",
+      2: "Weak",
+      3: "Good",
+      4: "Strong"
+    },
+    password_strength = document.getElementById('password-strength'),
+    meter = document.getElementsByTagName('meter');
 
+    if (/[A-Z]/.test(this.user.password)) count++;
+    if (/[a-z]/.test(this.user.password)) count++;
+    if (/\d/.test(this.user.password)) count++;
+    if (/[!@#\$%\^\&*\)\(+=._-]/.test(this.user.password)) count++;
+    
+    meter[0].value = count;
+    password_strength.innerHTML = strength[count];
   }
 
   matchPassword() {
