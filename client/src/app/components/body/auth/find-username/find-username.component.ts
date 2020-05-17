37518db54f9072eb3448d32f0cbfa6ebffcbc773
luclegen <luc.dev.sun@gmail.com>
2020-05-17 15:34:34 +0700

    Find Username Component: constructor, ngOnInit, onSubmit

--- client/src/app/components/body/auth/find-username/find-username.component.ts
+++ client/src/app/components/body/auth/find-username/find-username.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-find-username',
@@ -13,12 +16,24 @@ export class FindUsernameComponent implements OnInit {
 
   emailRegex;
 
-  constructor() { }
-
+  constructor(private titleService: Title, private authService: AuthService, private router: Router) {
+    this.titleService.setTitle('Find Username | Lmaci');
+    this.emailRegex = this.authService.emailRegex;
+  }
   ngOnInit(): void {
+    if (this.authService.getToken()) this.router.navigateByUrl('');
   }
 
   onSubmit(form: NgForm) {
+    this.authService.findUsername(form.value).subscribe(
+      res => {
+        alert(res['msg']);
+        this.router.navigateByUrl('reset-password/' + res['username']);
+      },
+      err => {
+        this.serverErrorMessages = err.error.msg;
+      }
+    );
   }
 
 }
