37bec33783b7faec66e094e67e72a9afe465e683
luclegen <luc.dev.sun@gmail.com>
2020-05-21 16:21:22 +0700

    Admin Controller: getUsers

---
+++ server/controllers/admin.controller.js
@@ -0,0 +1,8 @@
+const User = require('../models/user.model');
+
+module.exports.getUsers = (req, res) => {
+  User.find((err, users) => {
+    return users ? res.status(200).json({ users })
+                 : res.status(404).json({ msg: 'Users not found.' })
+  });
+}
