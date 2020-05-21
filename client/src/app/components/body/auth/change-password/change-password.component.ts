bc09b098afffd96ad45d6896a5651c6a26c6bfd3
luclegen <luc.dev.sun@gmail.com>
2020-05-21 09:42:59 +0700

    Change Password Component: View

--- client/src/app/components/body/auth/change-password/change-password.component.ts
+++ client/src/app/components/body/auth/change-password/change-password.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-change-password',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  user = {
+    password: '',
+    newPassword: '',
+    confirmPassword: ''
+  }
 
+  codeRegex;
+
+  serverErrorMessages: string;
+  successMessages: string;
+
+  constructor(private titleServer: Title, private authService: AuthService, private router: Router) {
+    this.titleServer.setTitle('Change Password | Lmaci');
+  }
+  
   ngOnInit(): void {
   }
 
+  checkStrengthPassword() {
+
+  }
+
+  matchPassword() {
+
+  }
+  
+  togglePassword() {
+
+  }
+
+  onSubmit(form: NgForm) {
+    
+  }
 }
