6f76418d19b9f6ad58a48b65160223aea6c95aa2
luclegen <luc.dev.sun@gmail.com>
2020-05-23 17:19:16 +0700

    Root Controller: removeAsAdmin

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -11,19 +11,6 @@ module.exports.getAdmins = (req, res) => {
   });
 }
 
-module.exports.removeAsAdmin = (req, res) => {
-  User.findOne({ username: req.params.username }, (err, admin) => {
-    if (admin) {
-      admin.role = 'user';
-
-      admin.save(err => {
-        return err ? res.status(400).json({ msg: 'Update is error.' })
-                 : res.status(200).json({ msg: 'Remove as admin was successfully.' });
-      });
-    } else return res.status(404).json({ msg: 'Admin not found.' });
-  });
-}
-
 module.exports.getUsers = (req, res) => {
   User.find({ role: 'user' }, (err, users) => {
     return users ? res.status(200).json({ users })
