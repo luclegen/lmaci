8d171c1c07f4cd459f674ee92a1f5b46e4d8dcd7
luclegen <luc.dev.sun@gmail.com>
2020-05-23 09:30:09 +0700

    Admin Controller: getAdmins

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -1,5 +1,16 @@
 const User = require('../models/user.model');
 
+module.exports.getAdmins = (req, res) => {
+  User.find({ role: 'root' }, (err, root) => {
+    if (root) {
+      User.find({ role: 'admin' }, (err, admins) => {
+        return users ? res.status(200).json({ root, admins })
+                     : res.status(404).json({ msg: 'Admins not found.' })
+      });
+    } else return res.status(404).json({ msg: 'Root not found.' })
+  });
+}
+
 module.exports.getUsers = (req, res) => {
   User.find({ role: 'user' }, (err, users) => {
     return users ? res.status(200).json({ users })
