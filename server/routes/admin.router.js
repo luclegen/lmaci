8a29e7a817f10f5ee82c6f27f87aa9105ea476e6
luclegen <luc.dev.sun@gmail.com>
2020-05-23 17:34:46 +0700

    Admin Router: GET removeAsAdmin

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -4,7 +4,7 @@ const express = require('express'),
 const adminController = require('../controllers/admin.controller');
 
 router.get('/admins', adminController.getAdmins);
-router.put('/remove-as-admin/:username', adminController.removeAsAdmin);
+router.get('/remove-as-admin/:username', adminController.removeAsAdmin);
 router.get('/users', adminController.getUsers);
 
 module.exports = router;
