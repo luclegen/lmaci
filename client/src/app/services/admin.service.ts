5d08a057afb3f239b925879a78aadeb3cb31e96e
luclegen <luc.dev.sun@gmail.com>
2020-05-23 09:56:08 +0700

    Admin Service: getAdmins

--- client/src/app/services/admin.service.ts
+++ client/src/app/services/admin.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //#region Http Methods
+
+  getAdmins() {
+    return this.http.get(environment.adminUrl + '/admins');
+  }
+
+  //#endregion Http Methods
+
 }
