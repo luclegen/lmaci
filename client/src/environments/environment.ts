b17469c065f225f985959f0ac9a43e77895b446e
luclegen <luc.dev.sun@gmail.com>
2020-05-23 09:51:56 +0700

    Environments: adminUrl

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   authUrl: 'http://localhost:3000/auth',
-  userUrl: 'http://localhost:3000/user'
+  userUrl: 'http://localhost:3000/user',
+  adminUrl: 'http://localhost:3000/admin'
 };
 
 /*
