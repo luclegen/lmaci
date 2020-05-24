eba8f360ed02ff9d150566fd031ba21de41e511a
luclegen <luc.dev.sun@gmail.com>
2020-05-24 10:21:25 +0700

    Users Component: View

--- client/src/app/components/body/admin/users/users.component.ts
+++ client/src/app/components/body/admin/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  users;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  viewProfile(form: NgForm) {
+
+  }
+
+  makeAdmin(form: NgForm) {
+
+  }
 }
