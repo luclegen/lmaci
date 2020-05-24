64c2b6a4f2b81f24ee33af7a0d7d81027c6ced6e
luclegen <luc.dev.sun@gmail.com>
2020-05-24 10:28:49 +0700

    Users Component: ngOnInit

--- client/src/app/components/body/admin/users/users.component.ts
+++ client/src/app/components/body/admin/users/users.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { AdminService } from 'src/app/services/admin.service';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -10,9 +12,17 @@ export class UsersComponent implements OnInit {
 
   users;
 
-  constructor() { }
+  constructor(private adminService: AdminService) { }
 
   ngOnInit(): void {
+    this.adminService.getUsers().subscribe(
+      res => {
+        this.users = res['users'];
+      },
+      err => {
+        alert(err.error.msg);
+      }
+    );
   }
 
   viewProfile(form: NgForm) {
