6a99c341286b4c6e6781fa8251d22883c88c1d48
luclegen <luc.dev.sun@gmail.com>
2020-05-24 11:12:35 +0700

    Admin Router: GET makeAdmin

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -6,5 +6,6 @@ const adminController = require('../controllers/admin.controller');
 router.get('/admins', adminController.getAdmins);
 router.get('/remove-as-admin/:username', adminController.removeAsAdmin);
 router.get('/users', adminController.getUsers);
+router.get('/make-admin', adminController.makeAdmin);
 
 module.exports = router;
