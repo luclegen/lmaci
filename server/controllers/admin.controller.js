67673f0bbf3b58e126d6e4c231b180210142414b
luclegen <luc.dev.sun@gmail.com>
2020-05-24 11:11:22 +0700

    Admin Controller: makeAdmin

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -30,3 +30,16 @@ module.exports.getUsers = (req, res) => {
                  : res.status(404).json({ msg: 'Users not found.' })
   });
 }
+
+module.exports.makeAdmin = (req, res) => {
+  User.findOne({ username: req.params.username }, (err, user) => {
+    if (user) {
+      user.role = 'admin';
+
+      user.save(err => {
+        return err ? res.status(400).json({ msg: 'Update is error.' })
+                 : res.status(200).json({ msg: 'Make admin was successfully.' });
+      });
+    } else return res.status(404).json({ msg: 'User not found.' });
+  });
+}
