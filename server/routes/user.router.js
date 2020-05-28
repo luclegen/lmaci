d2e713bb9fe38aa29eb2d43339df192ad4f7544e
luclegen <luc.dev.sun@gmail.com>
2020-05-28 16:49:07 +0700

    User Router

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const userController = require('../controllers/user.controller');
+const userCtrl = require('../controllers/user.controller');
 
-router.get('/:username', userController.get);
-
+router.get('/:username', userCtrl.get);
+    
 module.exports = router;
