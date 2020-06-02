83ce7043ac6279a5b9b723eb1646cda616c85ec3
luclegen <luc.dev.sun@gmail.com>
2020-06-02 16:47:30 +0700

    Admin Router: POST addProduct

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -15,4 +15,8 @@ router.get('/make-admin/:username', adminCtrl.makeAdmin);
 router.put('/search-users', adminCtrl.searchUsers);
 //#endregion Users
 
+//#region Products
+router.post('/add-product', adminCtrl.addProduct);
+//#endregion Products
+
 module.exports = router;
