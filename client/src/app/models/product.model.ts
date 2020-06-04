c3b675e962809973383f73f078c4f8ccaf782958
luclegen <luc.dev.sun@gmail.com>
2020-06-04 12:23:33 +0700

    Models: product

--- client/src/app/models/product.model.ts
+++ client/src/app/models/product.model.ts
@@ -1,8 +1,14 @@
+import { Type } from '../shared/type.enum';
+
 export class Product {
   name: string;
   status: string;
   quantity: { imported: number, exported: number };
   price: number;
   star: { number: number, countRate: number };
-  type: string;
+  type: Type;
+  description: string;
+  colors: string[];
+  technicalDetails: [];
+  post: string;
 }
