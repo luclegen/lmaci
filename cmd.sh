66557029a9c34b78e5a78c1b2710914ec2df9313
luclegen <luc.dev.sun@gmail.com>
2020-06-04 15:30:04 +0700

    Command

--- cmd.sh
+++ cmd.sh
@@ -9,6 +9,7 @@ sudo npm i -g nodemon
 ng new client
 clear; cd client; ng s -o
 ng g class user --type=model
+npm i --save @syncfusion/ej2-angular-richtexteditor @syncfusion/ej2-angular-popups @syncfusion/ej2-angular-buttons
 
 # Optional
 kill -9 $(lsof -t -i:4200); clear; ng s
