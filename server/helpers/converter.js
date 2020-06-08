64ab0fda07a5710bba14b24d7fade6236100a44c
luclegen <luc.dev.sun@gmail.com>
2020-06-08 15:37:39 +0700

    Helpers: Converter

--- server/helpers/converter.js
+++ server/helpers/converter.js
@@ -1,3 +1,3 @@
-module.exports.convertName = phrase => {
+module.exports.toName = phrase => {
   return phrase.trim().split(/\s+/).map(w => w[0].toUpperCase() + w.slice(1).toLowerCase()).join(' ');
 }
