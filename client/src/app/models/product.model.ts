64cb66f49738f0ca9f5923a48da1867b8fbf7e89
luclegen <luc.dev.sun@gmail.com>
2020-06-08 11:46:53 +0700

    Models: product

--- client/src/app/models/product.model.ts
+++ client/src/app/models/product.model.ts
@@ -2,12 +2,15 @@ export class Product {
   _id: string;
   name: string;
   status: string;
-  quantity: { imported: number, exported: number };
   price: number;
+  quantity: { imported: number, exported: number };
   star: { number: number, countRate: number };
   type: string;
-  description: string;
-  colors: string[];
+  colors: [];
+  styles: [];
+  editions: [];
+  capacitys: [];
   technicalDetails: [];
+  description: string;
   post: string;
 }
