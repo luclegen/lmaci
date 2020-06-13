485af0bf17e4c6a068ebfe07f29e4d71efcc279e
luclegen <luc.dev.sun@gmail.com>
2020-06-13 18:14:57 +0700

    Admin Controller: uploadProductImg

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -84,7 +84,7 @@ module.exports.createProduct = (req, res, next) => {
 }
 
 module.exports.uploadProductImg = (req, res) => {
-  Product.findByIdAndUpdate(req.params.id, { $set: { img: process.env.PRODUCT_IMG + req.params.id + '/' + req.params.id } }, { new: true }, (err, product) => {
+  Product.findByIdAndUpdate(req.params.id, { $set: { img: process.env.PRODUCT_IMG + req.params.id + '/' + req.params.id  + '.png' } }, { new: true }, (err, product) => {
     if (product) loader.uploadImg(req.body.file, process.env.PRODUCT_IMG_UPLOAD, req.params.id);
     else return res.status(404).json({ msg: 'Product not found.' });
   });
