fd02db9a8723b5aeb58e88394ed06eb5aed9d586
luclegen <luc.dev.sun@gmail.com>
2020-06-13 12:07:22 +0700

    Helpers: Loader

--- server/helpers/loader.js
+++ server/helpers/loader.js
@@ -1,8 +1,11 @@
+const fs = require("fs");
 
-module.exports.uploadImg = (url, namefile) => {
+module.exports.uploadImg = (url, path, filename) => {
   var base64Data = url.replace(/^data:image\/png;base64,/, "");
 
-  require("fs").writeFile('./uploads/avatars/' + namefile + ".png", base64Data, 'base64', function(err) {
+  if (!fs.existsSync(path)) fs.mkdirSync(path);
+
+  fs.writeFile(path + '/' + filename + ".png", base64Data, 'base64', function(err) {
     if (err) console.log(err);
   });
 }
