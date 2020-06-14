fe55e8f709fa0209600b332d9134b753724db1f6
luclegen <luc.dev.sun@gmail.com>
2020-06-14 14:05:47 +0700

    Admin Service: uploadProductImg

--- client/src/app/services/admin.service.ts
+++ client/src/app/services/admin.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { environment } from 'src/environments/environment';
 import { Product } from '../models/product.model';
@@ -55,6 +55,10 @@ export class AdminService {
 
   createProduct(product: Product) {
     return this.http.post(environment.adminUrl + '/create-product', product);
+  }
+
+  uploadProductImg(id: string, file: FormData) {
+    return this.http.post(environment.adminUrl + '/upload-product-img/' + id, file);
   }
   //#endregion Products
 
