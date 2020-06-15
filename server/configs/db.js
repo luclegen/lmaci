4ca0e6e70926cf15c8480565851037499ff81a8e
luclegen <luc.dev.sun@gmail.com>
2020-06-15 18:03:46 +0700

    Database Configuration

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
