d9283c024983ce4c09cb51c3bce22890742c598c
luclegen <luc.dev.sun@gmail.com>
2020-06-15 12:27:55 +0700

    Database Configuration

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, err => {
-  if (err) console.log('ERROR: MongoDB connection is failed: ' + JSON.stringify(err, undefined, 2));
-  else console.log('MongoDB connection succeeded.');
-});
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
 
 require('../models/user.model');
 require('../models/code.model');
