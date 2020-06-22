f92bf9769c3e0099ae8a1458ede70bc93d946e72
luclegen <luc.dev.sun@gmail.com>
2020-06-22 11:24:08 +0700

    Routes: UserComponent

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -4,7 +4,6 @@ import { AuthGuard } from './auth/auth.guard';
 
 import { HomeComponent } from './components/body/home/home.component';
 import { UserComponent } from './components/body/user/user.component';
-import { ProfileComponent } from './components/body/user/profile/profile.component';
 import { RegisterComponent } from './components/body/auth/register/register.component';
 import { LoginComponent } from './components/body/auth/login/login.component';
 import { ActiveComponent } from './components/body/auth/active/active.component';
