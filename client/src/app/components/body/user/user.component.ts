3ba34e7d193d8b156ab6e436bebe042b11ad0423
luclegen <luc.dev.sun@gmail.com>
2020-06-22 11:20:23 +0700

    User Component: ngOnInit

--- client/src/app/components/body/user/user.component.ts
+++ client/src/app/components/body/user/user.component.ts
@@ -15,12 +15,31 @@ export class UserComponent implements OnInit {
   title = '\'s Profile';
   
   constructor(private titleService: Title,
-    private route: ActivatedRoute, 
-    private authService: AuthService, 
-    private userService: UserService, 
-    private router: Router) { }
+              private route: ActivatedRoute, 
+              private authService: AuthService, 
+              private userService: UserService, 
+              private router: Router) { }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+      const username = this.route.snapshot.paramMap.get('username');
+      this.authService.getInfo().subscribe(
+        res => {
+          if(res['user'].role === 'root' || res['user'].role === 'admin' || res['user'].username === username) {
+            this.userService.getUser(username).subscribe(
+              res => {
+                this.userDetails = res['user'];
+                this.titleService.setTitle(this.userDetails.name.first + this.title);
+                this.userDetails.role = this.userDetails.role.split('')[0].toUpperCase() + this.userDetails.role.split('').slice(1).join('');
+                this.userDetails.gender = this.userDetails.gender.split('')[0].toUpperCase() + this.userDetails.gender.split('').slice(1).join('');
+              },
+              err => {
+                alert(err.error.msg);
+                this.router.navigateByUrl('');
+              }
+            );
+          } else this.router.navigateByUrl('');
+        }
+      );
+    }
+  
 }
