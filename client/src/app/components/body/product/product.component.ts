6d2c63b384383deda9c3861d8f468814ce478f1f
luclegen <luc.dev.sun@gmail.com>
2020-06-23 15:53:01 +0700

    Product Component: ngOnInit

--- client/src/app/components/body/product/product.component.ts
+++ client/src/app/components/body/product/product.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.sass']
 })
 export class ProductComponent implements OnInit {
-
+  counter = 1;
+  size = 0;
+  
   constructor() { }
 
   ngOnInit(): void {
+    const carouselContainer = document.querySelector('.carousel-container') as HTMLElement;
+    const carouselSlide = document.querySelector('.carousel-slide') as HTMLElement;
+
+    this.size = carouselSlide.offsetWidth;
+
+    carouselSlide.style.transform = 'translateX(' + (-this.size * this.counter) + 'px)';
+
   }
 
   prev() {
