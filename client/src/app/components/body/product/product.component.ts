96bdf49c866143f90547e3898b5fcb5d71b3d048
luclegen <luc.dev.sun@gmail.com>
2020-06-25 16:39:51 +0700

    Product Component: showGallery

--- client/src/app/components/body/product/product.component.ts
+++ client/src/app/components/body/product/product.component.ts
@@ -68,6 +68,29 @@ export class ProductComponent implements OnInit {
   }
 
   showGallery() {
-    alert();
+    const left = document.querySelector('.left') as HTMLElement;
+    const carouselContainer = document.querySelector('.carousel-container') as HTMLElement;
+    const carouselImages = document.querySelectorAll('.carousel-slide img') as NodeListOf<HTMLElement>;
+    const prevBtn = document.getElementById('prevBtn');
+    const nextBtn = document.getElementById('nextBtn');
+
+    left.style.position = 'absolute';
+    left.style.zIndex = '103';
+    left.style.top = '0';
+    left.style.background = 'black';
+    left.style.left = '-10%';
+    left.style.width = '100vw';
+    left.style.height = '100vh';
+
+    prevBtn.style.top = '45%';
+    prevBtn.style.left = '20%';
+
+    nextBtn.style.top = '45%';
+    nextBtn.style.right = '20%';
+
+    this.size = carouselImages[0].offsetWidth;
+
+    carouselContainer.style.width = this.size + 'px';
+    // carouselContainer.style.margin = '0';
   }
 }
