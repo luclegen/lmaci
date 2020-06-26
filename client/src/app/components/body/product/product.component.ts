9aed32d76b2535d17c59d311e0d1fe20345b079e
luclegen <luc.dev.sun@gmail.com>
2020-06-26 08:30:28 +0700

    Product Component: showGallery

--- client/src/app/components/body/product/product.component.ts
+++ client/src/app/components/body/product/product.component.ts
@@ -67,19 +67,19 @@ export class ProductComponent implements OnInit {
   }
 
   showGallery() {
-    const left = document.querySelector('.left') as HTMLElement;
+    const leftContainer = document.querySelector('.left-container') as HTMLElement;
     const carouselContainer = document.querySelector('.carousel-container') as HTMLElement;
     const carouselImages = document.querySelectorAll('.carousel-slide img') as NodeListOf<HTMLElement>;
     const prevBtn = document.getElementById('prev-btn');
     const nextBtn = document.getElementById('next-btn');
 
-    left.style.position = 'absolute';
-    left.style.zIndex = '103';
-    left.style.top = '0';
-    left.style.background = 'black';
-    left.style.left = '-10%';
-    left.style.width = '100vw';
-    left.style.height = '100vh';
+    leftContainer.style.position = 'absolute';
+    leftContainer.style.zIndex = '103';
+    leftContainer.style.top = '0';
+    leftContainer.style.background = 'black';
+    leftContainer.style.left = '-10%';
+    leftContainer.style.width = '100vw';
+    leftContainer.style.height = '100vh';
 
     prevBtn.style.top = '45%';
     prevBtn.style.left = '20%';
