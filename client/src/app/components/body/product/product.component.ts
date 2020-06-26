088551ecbe4fd4a7c42c17ba052d88b0241fba9d
luclegen <luc.dev.sun@gmail.com>
2020-06-26 11:21:37 +0700

    Product Component: showGallery

--- client/src/app/components/body/product/product.component.ts
+++ client/src/app/components/body/product/product.component.ts
@@ -81,6 +81,7 @@ export class ProductComponent implements OnInit {
   showGallery() {
     const leftContainer = document.querySelector('.left-container') as HTMLElement;
     const carouselContainer = document.querySelector('.carousel-container') as HTMLElement;
+    const carouselSlide = document.querySelector('.carousel-slide') as HTMLElement;
     const carouselImages = document.querySelectorAll('.carousel-slide img') as NodeListOf<HTMLElement>;
     const prevBtn = document.getElementById('prev-btn');
     const nextBtn = document.getElementById('next-btn');
@@ -88,20 +89,29 @@ export class ProductComponent implements OnInit {
     leftContainer.style.position = 'absolute';
     leftContainer.style.zIndex = '103';
     leftContainer.style.top = '0';
+    leftContainer.style.padding = '0';
     leftContainer.style.background = 'black';
     leftContainer.style.left = '-10%';
     leftContainer.style.width = '100vw';
     leftContainer.style.height = '100vh';
 
+    prevBtn.style.fill = 'white';
     prevBtn.style.top = '45%';
-    prevBtn.style.left = '20%';
+    prevBtn.style.left = '15%';
 
+    nextBtn.style.fill = 'white';
     nextBtn.style.top = '45%';
-    nextBtn.style.right = '20%';
+    nextBtn.style.left = '80%';
+
+    carouselSlide.style.transition = 'none';
+    carouselSlide.style.transform = 'translateX(' + (-this.size * 0) + 'px)';
 
     this.size = carouselImages[0].offsetWidth;
 
     carouselContainer.style.width = this.size + 'px';
+    carouselSlide.style.transform = 'translateX(' + (-this.size * this.counter) + 'px)';
+    // carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+
     // carouselContainer.style.margin = '0';
   }
 }
