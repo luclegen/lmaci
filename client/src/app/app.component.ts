7321adac5e4a38fccb522b81486ecfca43cf7912
luclegen <luc.dev.sun@gmail.com>
2020-06-28 10:51:40 +0700

    App Component: ngOnInit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  ngOnInit(): void {
+    const vpWith = document.documentElement.clientWidth;
+    const thead = document.querySelector('thead');
+    const tbody = document.querySelector('tbody');
+    
+    thead.style.fontSize = vpWith * 0.015 + 'px';
+    tbody.style.fontSize = vpWith * 0.0125 + 'px';
+  }
+
 }
