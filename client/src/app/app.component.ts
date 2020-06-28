a350f9ab58f84e4d47735239bc2d1f65b2df9e5b
luclegen <luc.dev.sun@gmail.com>
2020-06-28 17:13:40 +0700

    App Component: ngOnInit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,12 +9,12 @@ export class AppComponent {
   title = 'client';
 
   ngOnInit(): void {
-    const vpWith = document.documentElement.clientWidth;
+    const vpWidth = document.documentElement.clientWidth;
     const thead = document.getElementsByTagName('thead') as HTMLCollectionOf<HTMLTableSectionElement>;
     const tbody = document.getElementsByTagName('tbody') as HTMLCollectionOf<HTMLTableSectionElement>;
     
-    for (let i = 0; i < thead.length; i++) thead[i].style.fontSize = vpWith * 0.015 + 'px';
-    for (let i = 0; i < tbody.length; i++) tbody[i].style.fontSize = vpWith * 0.0125 + 'px';
+    for (let i = 0; i < thead.length; i++) thead[i].style.fontSize = vpWidth * 0.015 + 'px';
+    for (let i = 0; i < tbody.length; i++) tbody[i].style.fontSize = vpWidth * 0.0125 + 'px';
   }
 
 }
