2c8e601ce55e255d35d5df45f6b1ff6a1f02b49f
luclegen <luc.dev.sun@gmail.com>
2020-06-29 16:38:49 +0700

    Product Router: GET get

--- server/routes/product.router.js
+++ server/routes/product.router.js
@@ -1,4 +1,6 @@
-const express = require('express'),
+const express = require('express');
 const router = express.Router();
 
-router.get('/:id', adminCtrl.getProduct);
+const productCtrl = require('../controllers/product.controller');
+
+router.get('/:id', productCtrl.getProduct);
