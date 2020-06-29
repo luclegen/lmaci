ec4f04640f384f5f74582b246a77ae0209890b18
luclegen <luc.dev.sun@gmail.com>
2020-06-29 16:30:48 +0700

    Product Controller: get

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,8 @@
+const Product = require('../models/product.model');
+
+module.exports.get = (req, res) => {
+  Product.findById(req.params.id, (err, product) => {
+    return product ? res.status(200).json({ product: product })
+                    : res.status(404).json({ msg: 'Product not found.' });
+  });
+}
