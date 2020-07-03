bc79fc714260687b913183e6908257933f490eb9
luclegen <luc.dev.sun@gmail.com>
2020-07-03 16:20:54 +0700

    Product Component: View

--- client/src/app/components/body/product/product.component.ts
+++ client/src/app/components/body/product/product.component.ts
@@ -45,7 +45,7 @@ export class ProductComponent implements OnInit {
   }
 
   img: any = '';
-  imgs: any = '';
+  imgs;
   imageChangedEvent: any = '';
   croppedImage: any = '';
 
@@ -334,7 +334,7 @@ export class ProductComponent implements OnInit {
   }
 
   add() {
-    
+
   }
 
   //#endregion Image Cropper
