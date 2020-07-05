2a9654780b01f040b603303ef10de7800cab343c
luclegen <luc.dev.sun@gmail.com>
2020-07-05 11:46:28 +0700

    Product Router: PUT uploadImgs

--- server/routes/product.router.js
+++ server/routes/product.router.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const productCtrl = require('../controllers/product.controller');
+const transfer = require('../helpers/transfer');
 
 router.get('/:id', productCtrl.get);
-router.put('upload-imgs/:id', productCtrl.uploadImgs);
+router.put('upload-imgs/:id', transfer.array('imgs'), productCtrl.uploadImgs);
 
 module.exports = router;
