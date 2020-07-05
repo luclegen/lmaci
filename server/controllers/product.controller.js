8f5c646bb201bdc52182d333c461411231a062e2
luclegen <luc.dev.sun@gmail.com>
2020-07-05 22:02:43 +0700

    Product Controller: uploadImgs

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -16,16 +16,18 @@ module.exports.uploadImgs = (req, res) => {
 
   // console.log(req.body.imgs.slice(1));
 
-  let imgs = [ req.body.imgs[0] ];
+  let sliders = [], slider = [ req.body.imgs[0] ];
 
   for (const i of req.body.imgs.slice(1)) {
-    imgs.push(new Buffer.from(converter.base64ToJpeg(i), 'base64'));
+    slider.push(new Buffer.from(converter.base64ToJpeg(i), 'base64'));
   }
+
+  sliders.push(slider);
 
   // new Buffer.from(converter.base64ToJpeg(req.body.img), 'base64')
   // let imgs = { imgs: [] };
   
-  Product.findByIdAndUpdate(req.params.id, { $set: { imgs: imgs } }, { new: true }, (err, product) => {
+  Product.findByIdAndUpdate(req.params.id, { $set: { sliders: sliders } }, { new: true }, (err, product) => {
     return product ? res.status(200).json()
                    : res.status(404).json({ msg: 'Upload this product image failed.' });
   });
