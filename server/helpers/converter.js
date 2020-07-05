e2d5f4567c7178768fb7d72e33d2a56fe84274c1
luclegen <luc.dev.sun@gmail.com>
2020-07-05 17:50:41 +0700

    Helpers: Converter

--- server/helpers/converter.js
+++ server/helpers/converter.js
@@ -1,5 +1,7 @@
 module.exports.toName = phrase => phrase.trim().split(/\s+/).map(w => w[0].toUpperCase() + w.slice(1).toLowerCase()).join(' ');
 
-module.exports.base64ToImg = base64 => base64.replace(/^data:image\/png;base64,/, "");
+module.exports.base64ToPng = base64 => base64.replace(/^data:image\/png;base64,/, "");
+
+module.exports.base64ToJpeg = base64 => base64.replace(/^data:image\/jpeg;base64,/, "");
 
 module.exports.toKeyword = phrase => phrase.trim().split(/\s+/).map(w => w.toLowerCase()).join(' ');
