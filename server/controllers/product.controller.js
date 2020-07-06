3402d79a78290737f99799e2307629ca0c2bd3d8
luclegen <luc.dev.sun@gmail.com>
2020-07-06 11:39:47 +0700

    Product Service: uploadImgs

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -20,7 +20,7 @@ module.exports.uploadImgs = (req, res) => {
 
   Product.findById(req.params.id, (err, product) => {
     if (product) {
-      const sliders = [], slider = [ req.body.imgs[0] ];
+      const sliders = [], slider = [ req.body.imgs[0] ], replace = false;
 
       for (let i = 0; i < product.slidersPaths.length; i++) {
         const slider = [];
@@ -32,9 +32,13 @@ module.exports.uploadImgs = (req, res) => {
       for (const i of req.body.imgs.slice(1)) slider.push(new Buffer.from(converter.base64ToJpeg(i), 'base64'));
 
       for (let i = 0; i < product.sliders.length; i++) {
-        if (slider[0] == product.sliders[i][0]) sliders[i] = slider;
-        else sliders.push(slider);
+        if (slider[0] == product.sliders[i][0]) {
+          sliders[i] = slider;
+          replace = true;
+        }
       }
+      
+      if (!replace) sliders.push(slider);
        
       Product.findByIdAndUpdate(req.params.id, { $set: { sliders: sliders } }, { new: true }, (err, product) => {
         return product ? res.status(200).json({ msg: 'Upload this images is successfully.' })
