1e3ee918fb03e8bcc1e065c6ece483c3bf9520aa
luclegen <luc.dev.sun@gmail.com>
2020-07-06 09:07:15 +0700

    Product Controller: uploadImgs

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,3 +1,5 @@
+const ObjectId = require('mongoose').Types.ObjectId;
+
 const Product = require('../models/product.model');
 
 const converter = require('../helpers/converter');
@@ -10,25 +12,32 @@ module.exports.get = (req, res) => {
 }
 
 module.exports.uploadImgs = (req, res) => {
-  // console.log(req.params.id);
-  
-  // console.log(req.body.imgs);
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).json({ msg: `No record with given id: ${req.params.id}` });
 
-  // console.log(req.body.imgs.slice(1));
+  Product.findById(req.params.id, (err, product) => {
+    if (product) {
+      let sliders = product.sliders, slider = [ req.body.imgs[0] ];
 
-  let sliders = [], slider = [ req.body.imgs[0] ];
+      for (let i = 0; i < product.sliders.length; i++) {
+        console.log(product.sliders[i][0] == slider[0]);
+        if (product.sliders[i][0] == slider[0]) {
 
-  for (const i of req.body.imgs.slice(1)) {
-    slider.push(new Buffer.from(converter.base64ToJpeg(i), 'base64'));
-  }
+        }
+        // for (let j = 0; j < product.sliders[i].length; j++) {
+        //   
+        // }
+      }
 
-  sliders.push(slider);
+      // if () {
 
-  // new Buffer.from(converter.base64ToJpeg(req.body.img), 'base64')
-  // let imgs = { imgs: [] };
-  
-  Product.findByIdAndUpdate(req.params.id, { $set: { sliders: sliders } }, { new: true }, (err, product) => {
-    return product ? res.status(200).json()
-                   : res.status(404).json({ msg: 'Upload this images failed.' });
+      // } else for (const i of req.body.imgs.slice(1)) slider.push(new Buffer.from(converter.base64ToJpeg(i), 'base64'));
+      // sliders.push(slider);
+    } else res.status(404).json({ msg: 'Product not found.' });
   });
+  
+  // Product.findByIdAndUpdate(req.params.id, { $set: { sliders: sliders } }, { new: true }, (err, product) => {
+  //   return product ? res.status(200).json()
+  //                  : res.status(404).json({ msg: 'Upload this images failed.' });
+  // });
 }
