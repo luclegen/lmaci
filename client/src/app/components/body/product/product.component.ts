8e63b2fde71a930952ce77a73b7df73ca7db7ad4
luclegen <luc.dev.sun@gmail.com>
2020-07-06 16:45:33 +0700

    Product Component: saveSlider

--- client/src/app/components/body/product/product.component.ts
+++ client/src/app/components/body/product/product.component.ts
@@ -392,32 +392,36 @@ export class ProductComponent implements OnInit {
   }
 
   saveSlider() {
-    this.authService.getInfo().subscribe(res => {
-      if (res['user'].role == 'root' || res['user'].role === 'admin') {
-        const formData = new FormData();
+    console.log(this.isSaveImgs());
+      
+    // if (this.isSaveImgs()) {
+    //   this.authService.getInfo().subscribe(res => {
+    //     if (res['user'].role == 'root' || res['user'].role === 'admin') {
+    //       const formData = new FormData();
 
-        formData.append('imgs', this.order.color.value);
-        for(let img of this.imgs) formData.append('imgs', img);
+    //       formData.append('imgs', this.order.color.value);
+    //       for(let img of this.imgs) formData.append('imgs', img);
 
-        this.productService.uploadImgs(this.id, formData).subscribe(
-          res => {
-            alert(res['msg']);
-            this.productService.getProduct(this.id).subscribe(
-              res => {
-                this.product = res['product'];
-              },
-              err => {
-                alert(err.error.msg);
-                this.router.navigateByUrl('');
-              }
-            );
-          },
-          err => {
-            alert(err.error.msg);
-          }
-        );
-      } else this.router.navigateByUrl('');
-    });
+    //       this.productService.uploadImgs(this.id, formData).subscribe(
+    //         res => {
+    //           alert(res['msg']);
+    //           this.productService.getProduct(this.id).subscribe(
+    //             res => {
+    //               this.product = res['product'];
+    //             },
+    //             err => {
+    //               alert(err.error.msg);
+    //               this.router.navigateByUrl('');
+    //             }
+    //           );
+    //         },
+    //         err => {
+    //           alert(err.error.msg);
+    //         }
+    //       );
+    //     } else this.router.navigateByUrl('');
+    //   });
+    // }
   }
 
   //#endregion Slider Editor
