24915242c96011f405ca8226dd585be919481405
luclegen <luc.dev.sun@gmail.com>
2020-07-08 10:06:14 +0700

    Product Component: reset

--- client/src/app/components/body/product/product.component.ts
+++ client/src/app/components/body/product/product.component.ts
@@ -397,17 +397,10 @@ export class ProductComponent implements OnInit {
   }
 
   reset() {
-    const carouselSlide = document.querySelector('.carousel-slide') as HTMLElement;
-    
     this.order.preview.name = this.order.color.name;
     this.order.preview.value = this.order.color.value;
-
-    this.imgs = [];
-
-    for (const slider of this.product.slidersPaths) if (slider[0] == this.order.preview.value) this.imgs = slider.slice(1);
-
-    carouselSlide.style.transition = 'none';
-    carouselSlide.style.transform = 'translateX(' + (-this.size * this.counter) + 'px)';
+    
+    this.reloadSlider();
   }
 
   //#endregion Order
