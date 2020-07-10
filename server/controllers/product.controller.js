3e70bbe849e3aa2aea6da013967384614e3b83b4
luclegen <luc.dev.sun@gmail.com>
2020-07-10 15:25:06 +0700

    Product Controller: post

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -53,7 +53,12 @@ module.exports.post = (req, res) => {
   if (!ObjectId.isValid(req.params.id))
     return res.status(400).json({ msg: `No record with given id: ${req.params.id}` });
 
-  Product.findByIdAndUpdate(req.params.id, { $set: { post: req.body.post } }, { new: true }, (err, product) => {
+    let post = {
+      content: req.body.content,
+      dateModified: req.body.dateModified
+    };
+
+  Product.findByIdAndUpdate(req.params.id, { $set: { post: post } }, { new: true }, (err, product) => {
     return product ? product.hasOwnProperty('post') ? res.status(200).json({ msg: 'Update this post is successfully.' })
                                                     : res.status(200).json({ msg: 'Post this product is successfully.' })
                    : res.status(404).json({ msg: 'Product not found.' });
