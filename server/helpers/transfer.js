f97efc5639fea09b789696c1872ec531fe1c4e7c
luclegen <luc.dev.sun@gmail.com>
2020-07-11 17:06:39 +0700

    Transfer Heper

--- server/helpers/transfer.js
+++ server/helpers/transfer.js
@@ -1,3 +1,11 @@
 const multer = require('multer');
 
+module.exports.getImages = (imgDir, callback) => {
+  let files = [];
+  fs.readdir(imgDir, function (err, ls) {
+    for (let i = 0; i < ls.length; i++) files.push(ls[i]);
+    callback(err, files);
+  });
+}
+
 module.exports = multer();
