631333d9ed9b3f52f60210aa77376098c0a45137
luclegen <luc.dev.sun@gmail.com>
2020-07-15 22:09:44 +0700

    Login Component: constructor

--- client/src/app/components/body/auth/login/login.component.ts
+++ client/src/app/components/body/auth/login/login.component.ts
@@ -5,6 +5,7 @@ import { NgForm } from '@angular/forms';
 
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
+import { HelperService } from 'src/app/services/helper.service';
 
 @Component({
   selector: 'app-login',
@@ -21,9 +22,12 @@ export class LoginComponent implements OnInit {
   usernameRegex;
   serverErrorMessages: string;
 
-  constructor(private titleService: Title, private authService: AuthService, private userService: UserService, private router: Router) {
+  constructor(private titleService: Title,
+              private helperService: HelperService,
+              private authService: AuthService,
+              private router: Router) {
     this.titleService.setTitle(this.title);
-    this.usernameRegex = this.authService.usernameRegex;
+    this.usernameRegex = this.helperService.usernameRegex;
   }
 
   ngOnInit(): void {
