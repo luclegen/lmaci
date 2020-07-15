2dad947b4d6743a3953f46e4691a8cd1ca516ac3
luclegen <luc.dev.sun@gmail.com>
2020-07-15 22:05:35 +0700

    User Service

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -11,11 +11,6 @@ export class UserService {
   
   noAuthHeader = { headers: new HttpHeaders({ 'noAuth': 'True' }) };
   
-  usernameRegex = /^(?=[a-zA-Z0-9._]{1,20}$)/;
-  codeRegex = /^\d{6}$/;
-  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  mobileNumberRegex = /^(\+\d{1,3}[- ]?)?\d{10}$/;
-  
   constructor(private http: HttpClient) { }
 
   //#region Http Methods
