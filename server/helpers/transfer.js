51d8554782346b6d918260333c9c419165782032
luclegen <luc.dev.sun@gmail.com>
2020-07-15 16:40:25 +0700

    Transfer Heper

--- server/helpers/transfer.js
+++ server/helpers/transfer.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const fs = require("fs");
 
 module.exports.getImages = (imgDir, callback) => {
   let files = [];
@@ -8,10 +9,14 @@ module.exports.getImages = (imgDir, callback) => {
   });
 }
 
-module.exports.upload = () => {
+module.exports.upload = (path, dir) => {
   const storage = multer.diskStorage({
     destination: (req, file, callBack) => {
-      callBack(null, 'uploads')
+      if (!fs.existsSync(path + '/' + req.params.id)) fs.mkdirSync(path + '/' + req.params.id);
+      if (!fs.existsSync(path + '/' + req.params.id + '/' + dir)) fs.mkdirSync(path + '/' + req.params.id + '/' + dir);
+      if (!fs.existsSync(path + '/' + req.params.id + '/' + dir + '/' + req.body.index)) fs.mkdirSync(path + '/' + req.params.id + '/' + dir + '/' + req.body.index);
+
+      callBack(null, path + '/' + req.params.id + '/' + dir + '/' + req.body.index)
     },
     filename: (req, file, callBack) => {
       callBack(null, `${file.originalname}`)
@@ -20,5 +25,3 @@ module.exports.upload = () => {
   
   return multer({ storage: storage });
 }
-
-// module.exports = multer();
