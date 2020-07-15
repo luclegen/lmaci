9c74548b3d7bebc95e85a6a7479ea4abe83431ed
luclegen <luc.dev.sun@gmail.com>
2020-07-15 21:53:54 +0700

    Helper Service: Regex

--- client/src/app/services/helper.service.ts
+++ client/src/app/services/helper.service.ts
@@ -5,5 +5,13 @@ import { Injectable } from '@angular/core';
 })
 export class HelperService {
 
+  positiveNumberRegex = /^\d*[1-9]\d*$/;
+  NotNegativeNumberRegex = /^\d*[0-9]\d*$/;
+  
+  usernameRegex = /^(?=[a-zA-Z0-9._]{1,20}$)/;
+  codeRegex = /^\d{6}$/;
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  mobileNumberRegex = /^(\+\d{1,3}[- ]?)?\d{10}$/;
+  
   constructor() { }
 }
