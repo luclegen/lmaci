a253e4bc3ce74466fb7638b22390cf58e0cff6f6
luclegen <luc.dev.sun@gmail.com>
2020-07-15 10:26:31 +0700

    Product Router: PUT review

--- server/routes/product.router.js
+++ server/routes/product.router.js
@@ -7,6 +7,6 @@ const transfer = require('../helpers/transfer');
 router.get('/:id', productCtrl.get);
 router.put('/upload-imgs/:id', transfer.array('imgs'), productCtrl.uploadImgs);
 router.put('/post/:id', productCtrl.post);
-router.put('/review/:id', productCtrl.review);
+router.put('/review/:id', transfer.upload().array('star'), transfer.upload().array('content'), transfer.upload().array('files') , productCtrl.review);
 
 module.exports = router;
