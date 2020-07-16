ba72a86b5b44c4e7fdb508f74ab3f2743e70a5aa
luclegen <luc.dev.sun@gmail.com>
2020-07-16 09:53:38 +0700

    Helper Service: base64ToBlob

--- client/src/app/services/helper.service.ts
+++ client/src/app/services/helper.service.ts
@@ -20,4 +20,13 @@ export class HelperService {
     return nameOut[0].toLowerCase() + nameOut.slice(1);
   }
 
+  base64ToBlob(base64: string, type: string) {
+    const byteString = window.atob(base64);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const int8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < byteString.length; i++) int8Array[i] = byteString.charCodeAt(i);
+    const blob = new Blob([ int8Array ], { type: 'image/' + type });
+    return blob;
+  }
+
 }
