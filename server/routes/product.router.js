785d12e94dc4d2dd1d205b2731d8ec0fc76d263b
luclegen <luc.dev.sun@gmail.com>
2020-07-17 21:21:28 +0700

    Product Router: PUT uploadImgs

--- server/routes/product.router.js
+++ server/routes/product.router.js
@@ -5,7 +5,7 @@ const productCtrl = require('../controllers/product.controller');
 const transfer = require('../helpers/transfer');
 
 router.get('/:id', productCtrl.get);
-router.put('/upload-imgs/:id', transfer.transfer().array('imgs'), productCtrl.uploadImgs);
+router.put('/upload-imgs/:id', transfer.upload('uploads/img/product', 'slider').array('files'), productCtrl.uploadImgs);
 router.put('/post/:id', productCtrl.post);
 router.put('/review/:id', transfer.upload('uploads/img/product', 'review').array('files'), productCtrl.review);
 
