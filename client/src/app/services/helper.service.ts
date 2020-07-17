55d6e2e9b26cc26d0df6ef3452b929b8db18b485
luclegen <luc.dev.sun@gmail.com>
2020-07-17 09:02:54 +0700

    Helper Service: USDcurrency

--- client/src/app/services/helper.service.ts
+++ client/src/app/services/helper.service.ts
@@ -40,4 +40,5 @@ export class HelperService {
   USDcurrency(number) {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(number);
   }
+  
 }
