23294c3adedcd56a5b2e83aa698b90991e51ecfd
luclegen <luc.dev.sun@gmail.com>
2020-07-17 22:23:36 +0700

    Product Component: savePost

--- client/src/app/components/body/product/product.component.ts
+++ client/src/app/components/body/product/product.component.ts
@@ -742,26 +742,26 @@ export class ProductComponent implements OnInit {
     this.authService.getInfo().subscribe(res => {
       if (res['user'].role == 'root' || res['user'].role === 'admin') {
         alert(JSON.stringify(this.post));
-        // this.post.dateModified = Date.now();
-        // this.productService.post(this.id, this.post).subscribe(
-        //   res => {
-        //     alert(res['msg']);
-        //     this.productService.getProduct(this.id).subscribe(
-        //       res => {
-        //         this.product = res['product'];
+        this.post.dateModified = Date.now();
+        this.productService.post(this.id, this.post).subscribe(
+          res => {
+            alert(res['msg']);
+            this.productService.getProduct(this.id).subscribe(
+              res => {
+                this.product = res['product'];
         
-        //         this.initPost();
-        //       },
-        //       err => {
-        //         alert(err.error.msg);
-        //         this.router.navigateByUrl('');
-        //       }
-        //     );
-        //   },
-        //   err => {
-        //     alert(err.error.msg);
-        //   }
-        // );
+                this.initPost();
+              },
+              err => {
+                alert(err.error.msg);
+                this.router.navigateByUrl('');
+              }
+            );
+          },
+          err => {
+            alert(err.error.msg);
+          }
+        );
       } else this.router.navigateByUrl('');
     });
   }
