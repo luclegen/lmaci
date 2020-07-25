e82d05ab81ceef1a337b22d195969ae49a91a9b6
luclegen <luc.dev.sun@gmail.com>
2020-07-25 10:27:14 +0700

    Helpers: Code Generator

--- server/helpers/generator.js
+++ server/helpers/generator.js
@@ -1,5 +1,5 @@
 module.exports.generateCode = length => {
   let code = '', characters = '0123456789';
-  for (let i = 0; i < length; i++) code += characters.charAt(Math.floor(Math.random() * characters.length));
+  for (let i = 0; i < length; i++) code += characters[Math.floor(Math.random() * characters.length)];
   return code;
 }
