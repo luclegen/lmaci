4abb977a45fc209b57d320eb60130a87186d45c7
luclegen <luc.dev.sun@gmail.com>
2020-07-29 11:44:41 +0700

    Product Component: selectImg

--- client/src/app/components/body/product/product.component.ts
+++ client/src/app/components/body/product/product.component.ts
@@ -500,6 +500,8 @@ export class ProductComponent implements OnInit {
 
     carouselSlide.style.transform = 'translateX(' + (-this.size * this.counter) + 'px)';
     galleryFrame.style.transform = 'translateX(' + ((this.sizeFrame + 1) * this.counter + 1) + 'px)';
+    
+    this.scrollFrame();
   }
 
   scrollFrame() {
