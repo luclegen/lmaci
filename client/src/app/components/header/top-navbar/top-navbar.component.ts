bc34364f89fa4e4dd3bbfa40dbcbb5d92c7e9096
luclegen <luc.dev.sun@gmail.com>
2020-08-02 14:27:52 +0700

    Top Navbar Component: ngOnInit

--- client/src/app/components/header/top-navbar/top-navbar.component.ts
+++ client/src/app/components/header/top-navbar/top-navbar.component.ts
@@ -19,7 +19,10 @@ export class TopNavbarComponent implements OnInit {
         res => {
           this.userDetails = res['user'];
         }, err => {
-          if (!err.error.auth) this.authService.removeToken();
+          if (err.status === 440) {
+            if (confirm('Your session has expired and must log in again.\nDo you want to login again?')) window.open('/login');
+            else this.authService.removeToken();
+          } else this.authService.removeToken();
         }
       );
     }
