ded00817dc89594c5ea0a7fbf03d68b07ff85700
luclegen <luc.dev.sun@gmail.com>
2020-08-02 15:48:53 +0700

    Product Component: cancelSaveSlider

--- client/src/app/components/body/product/product.component.ts
+++ client/src/app/components/body/product/product.component.ts
@@ -668,10 +668,18 @@ export class ProductComponent implements OnInit {
   }
 
   cancelSaveSlider() {
-    this.authService.getInfo().subscribe(res => {
-      if (res['user'].role == 'root' || res['user'].role === 'admin') this.onCheckColor(this.order.color);
-      else this.router.navigateByUrl('');
-    });
+    this.authService.getInfo().subscribe(
+      res => {
+        if (res['user'].role == 'root' || res['user'].role === 'admin') this.onCheckColor(this.order.color);
+        else this.router.navigateByUrl('');
+      },
+      err => {
+        if (err.status == 440) {
+          if (confirm('Your session has expired and must log in again.\nDo you want to login again?')) window.open('/login');
+          else this.authService.removeToken();
+        } else this.authService.removeToken();
+      }
+    );
   }
 
   //#endregion Slider Editor
