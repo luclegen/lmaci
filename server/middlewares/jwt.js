3162ebbf4c38ab9d67a0cf1e939c40abd84e92cb
luclegen <luc.dev.sun@gmail.com>
2020-08-02 14:26:35 +0700

    Middlewares: Json Web Token

---
+++ server/middlewares/jwt.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+module.exports.verifyJwtToken = async (req, res, next) => {
+  if ('authorization' in req.headers) {
+    const token = req.headers['authorization'].split(' ')[1];
+
+    if (token) {
+      try {
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+        if (decoded) {
+          req._id = decoded._id;
+          next();
+        } else return res.status(500).send({ auth: false, msg: 'Token authentication failed.' });
+      } catch (err) {
+        return res.status(440).send({ auth: false, msg: 'Token has expired.' });
+      }
+    } else return res.status(403).send({ auth: false, msg: 'No token provided.' });
+  }
+}
