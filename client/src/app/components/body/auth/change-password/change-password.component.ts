6ac44edab6a96260d02c609382df1ce85ecfe2a8
luclegen <luc.dev.sun@gmail.com>
2020-08-03 09:10:52 +0700

    Change Password Component: ngOnInit

--- client/src/app/components/body/auth/change-password/change-password.component.ts
+++ client/src/app/components/body/auth/change-password/change-password.component.ts
@@ -31,10 +31,7 @@ export class ChangePasswordComponent implements OnInit {
         if (!res['user'].activated) this.router.navigateByUrl('active');
       },
       err => {
-        if (err.status == 440) {
-          if (confirm('Your session has expired and must log in again.\nDo you want to login again?')) window.open('/login');
-          else this.authService.removeToken();
-        } else this.authService.removeToken();
+        if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login');
       }
     );
   }
