aff216c9607575b760b8fa253f87f2fa60b39e2a
luclegen <luc.dev.sun@gmail.com>
2020-08-03 15:53:31 +0700

    Product Controller: uploadImgs

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -13,46 +13,47 @@ module.exports.get = async (req, res) => {
                  : res.status(404).json({ msg: 'Product not found.' });
 }
 
-module.exports.uploadImgs = (req, res) => {
+module.exports.uploadImgs = async (req, res) => {
   if (!ObjectId.isValid(req.params.id))
     return res.status(400).json({ msg: `No record with given id: ${req.params.id}` });
 
-  Product.findById(req.params.id, (err, product) => {
-    if (product) {
-      const paths = JSON.parse(req.body.paths);
-      let replace = false;
-      const sliders = product.sliders ? product.sliders : [];
-      const indexs = sliders.filter(s => s.color == req.body.color).length ? sliders.filter(s => s.color == req.body.color)[0].imgs.map(i => i.index).concat(JSON.parse(req.body.indexs)) : JSON.parse(req.body.indexs);
-      const slider = {
-        color: req.body.color,
-        imgs: []
-      };
-      
-      paths.forEach(path => {
-        const img = {
-          index: indexs.filter(index => (new RegExp(req.body.color.replace(/#/, '') + '/' + index + '.jpeg')).test(path))[0],
-          path: path
-        }
-        indexs.splice(indexs.indexOf(img.index), 1)
-        slider.imgs.push(img);
-      })
+  const product = await Product.findById(req.params.id);
 
-      sliders.forEach(s => {
-        if (s.color == req.body.color) {
-          sliders[sliders.indexOf(s)] = slider;
-          replace = true;
-        }
-      });
-      if (!replace) sliders.push(slider);
+  if (product) {
+    const paths = JSON.parse(req.body.paths);
+    let replace = false;
+    const sliders = product.sliders ? product.sliders : [];
+    const indexs = sliders.filter(s => s.color == req.body.color).length ? sliders.filter(s => s.color == req.body.color)[0].imgs.map(i => i.index).concat(JSON.parse(req.body.indexs)) : JSON.parse(req.body.indexs);
+    const slider = {
+      color: req.body.color,
+      imgs: []
+    };
+    
+    paths.forEach(path => {
+      const img = {
+        index: indexs.filter(index => (new RegExp(req.body.color.replace(/#/, '') + '/' + index + '.jpeg')).test(path))[0],
+        path: path
+      }
+      indexs.splice(indexs.indexOf(img.index), 1)
+      slider.imgs.push(img);
+    })
 
-      indexs.forEach(i => rimraf.sync('uploads/img/product/' + req.params.id + '/slider/' + req.body.color.replace(/#/, '') + '/' + i + '.jpeg'));
-      
-      Product.findByIdAndUpdate(req.params.id, { $set: { sliders: sliders } }, { new: true }, (err, product) => {
-        return product ? res.status(200).json({ msg: 'Upload this images is successfully.' })
-                      : res.status(404).json({ msg: 'Upload this images failed!' });
-      });
-    } else res.status(404).json({ msg: 'Product not found.' });
-  });
+    sliders.forEach(s => {
+      if (s.color == req.body.color) {
+        sliders[sliders.indexOf(s)] = slider;
+        replace = true;
+      }
+    });
+    if (!replace) sliders.push(slider);
+
+    indexs.forEach(i => rimraf.sync('uploads/img/product/' + req.params.id + '/slider/' + req.body.color.replace(/#/, '') + '/' + i + '.jpeg'));
+    
+    const product1 = await Product.findByIdAndUpdate(req.params.id, { $set: { sliders: sliders } }, { new: true });
+
+    return product1 ? res.status(200).json({ msg: 'Upload this images is successfully.' })
+                    : res.status(404).json({ msg: 'Upload this images failed!' });
+
+  } else res.status(404).json({ msg: 'Product not found.' });
 }
 
 module.exports.post = (req, res) => {
