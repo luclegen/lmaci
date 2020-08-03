a7b6236d5f3769da0357eabfe7b2e2706fad7b34
luclegen <luc.dev.sun@gmail.com>
2020-08-03 09:12:45 +0700

    Change Password Component: onSubmit

--- client/src/app/components/body/auth/change-password/change-password.component.ts
+++ client/src/app/components/body/auth/change-password/change-password.component.ts
@@ -94,22 +94,10 @@ export class ChangePasswordComponent implements OnInit {
     if (this.matchPassword()) {
       this.authService.getInfo().subscribe(
         res => {
-          if (res['user'].activated) {
-            this.authService.changePassword(this.authService.getId(), form.value).subscribe(
-              res => {
-                this.successMessages = res['msg'];
-              },
-              err => {
-                this.serverErrorMessages = err.error.msg;
-              }
-            );
-          }
+          if (res['user'].activated) this.authService.changePassword(this.authService.getId(), form.value).subscribe(res => this.successMessages = res['msg'], err => this.serverErrorMessages = err.error.msg);
         },
         err => {
-          if (err.status == 440) {
-            if (confirm('Your session has expired and must log in again.\nDo you want to login again?')) window.open('/login');
-            else this.authService.removeToken();
-          } else this.authService.removeToken();
+          if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login');
         }
       );
     }
