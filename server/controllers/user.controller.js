efe6ac3fcf713dc2cff8fbdef13610cb3b595d2b
luclegen <luc.dev.sun@gmail.com>
2020-08-03 16:07:19 +0700

    User Controller: get

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,8 +1,8 @@
 const User = require('../models/user.model');
 
-module.exports.get = (req, res) => {
-  User.findOne({ username: req.params.username }, (err, user) => {
-    return user ? res.status(200).json({ user })
-                : res.status(404).json({ msg: 'User not found.' });
-  });
+module.exports.get = async (req, res) => {
+  const user = await User.findOne({ username: req.params.username });
+
+  return user ? res.status(200).json({ user })
+              : res.status(404).json({ msg: 'User not found.' });
 }
