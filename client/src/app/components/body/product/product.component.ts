4a28031bfc0be657bf6566c9268a1baddefcc903
luclegen <luc.dev.sun@gmail.com>
2020-08-03 09:21:42 +0700

    Product Component: cancelSavePost

--- client/src/app/components/body/product/product.component.ts
+++ client/src/app/components/body/product/product.component.ts
@@ -845,13 +845,9 @@ export class ProductComponent implements OnInit {
     this.authService.getInfo().subscribe(
       res => {
         if (res['user'].role == 'root' || res['user'].role === 'admin') this.initPost();
-        else this.router.navigateByUrl('');
       },
       err => {
-        if (err.status == 440) {
-          if (confirm('Your session has expired and must log in again.\nDo you want to login again?')) window.open('/login');
-          else this.authService.removeToken();
-        } else this.authService.removeToken();
+        if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login');
       }
     );
   }
