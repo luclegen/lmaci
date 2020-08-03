7b6f30ed81fac46978d77db776db0a7f6d0a02e6
luclegen <luc.dev.sun@gmail.com>
2020-08-03 08:36:03 +0700

    Admins Component: onSubmit

--- client/src/app/components/body/admin/admins/admins.component.ts
+++ client/src/app/components/body/admin/admins/admins.component.ts
@@ -94,13 +94,10 @@ export class AdminsComponent implements OnInit {
               this.root = null;
               this.admins = res['admins'];
             });
-          } else this.router.navigateByUrl('');
+          }
         },
         err => {
-          if (err.status == 440) {
-            if (confirm('Your session has expired and must log in again.\nDo you want to login again?')) window.open('/login');
-            else this.authService.removeToken();
-          } else this.authService.removeToken();
+          if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login');
         }
       );
     }
