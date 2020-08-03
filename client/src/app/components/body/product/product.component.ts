3f9d66716743b2a61210dbc3d482b858af28bd8b
luclegen <luc.dev.sun@gmail.com>
2020-08-03 09:20:49 +0700

    Product Component: cancelSaveSlider

--- client/src/app/components/body/product/product.component.ts
+++ client/src/app/components/body/product/product.component.ts
@@ -663,13 +663,9 @@ export class ProductComponent implements OnInit {
     this.authService.getInfo().subscribe(
       res => {
         if (res['user'].role == 'root' || res['user'].role === 'admin') this.onCheckColor(this.order.color);
-        else this.router.navigateByUrl('');
       },
       err => {
-        if (err.status == 440) {
-          if (confirm('Your session has expired and must log in again.\nDo you want to login again?')) window.open('/login');
-          else this.authService.removeToken();
-        } else this.authService.removeToken();
+        if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login');
       }
     );
   }
