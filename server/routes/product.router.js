de3fd98c3d0defcefc61e517c11892193521e664
luclegen <luc.dev.sun@gmail.com>
2020-08-19 11:19:16 +0700

    Product Router: PUT deleteComment

--- server/routes/product.router.js
+++ server/routes/product.router.js
@@ -10,5 +10,6 @@ router.put('/post/:id', productCtrl.post);
 router.put('/review/:id', transfer.upload('uploads/img/product', 'review').array('files'), productCtrl.review);
 router.put('/delete-review/:id', productCtrl.deleteReview);
 router.put('/comment/:id', transfer.upload('uploads/img/product', 'comment').array('files'), productCtrl.comment);
+router.put('/delete-comment/:id', productCtrl.deleteComment);
 
 module.exports = router;
