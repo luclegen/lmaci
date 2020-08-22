c495450ac9c083c95a5e7b8470bc4ea95f1d710f
luclegen <luc.dev.sun@gmail.com>
2020-08-22 16:24:53 +0700

    Products Component: onEditColor

--- client/src/app/components/body/admin/products/products.component.ts
+++ client/src/app/components/body/admin/products/products.component.ts
@@ -495,8 +495,7 @@ export class ProductsComponent implements OnInit {
   //#region Edit
 
   onEditColor(c: Object) {
-    this.color = Object(c);
-    this.colorSeleted = Object(c);
+    this.color = this.colorSeleted = Object(c);
     this.colorTmp = JSON.parse(JSON.stringify(c));
   }
 
