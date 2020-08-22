9db37efacf3320e131b431fcac64af72fd1cd7e5
luclegen <luc.dev.sun@gmail.com>
2020-08-22 17:21:51 +0700

    Product Component: savePost

--- client/src/app/components/body/product/product.component.ts
+++ client/src/app/components/body/product/product.component.ts
@@ -850,20 +850,14 @@ export class ProductComponent implements OnInit {
           
                   this.ngOnInit();
                 },
-                err => {
-                  alert(err.error.msg);
-                }
+                err => alert(err.error.msg)
               );
             },
-            err => {
-              alert(err.error.msg);
-            }
+            err => alert(err.error.msg)
           );
         }
       },
-      err => {
-        if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login');
-      }
+      err => { if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login'); }
     );
   }
 
