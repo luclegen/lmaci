cc71424e2170375828ee53814ca8179363bb410a
luclegen <luc.dev.sun@gmail.com>
2020-08-22 17:07:12 +0700

    Change Email Component: onSubmit

--- client/src/app/components/body/auth/change-email/change-email.component.ts
+++ client/src/app/components/body/auth/change-email/change-email.component.ts
@@ -44,9 +44,7 @@ export class ChangeEmailComponent implements OnInit {
           );
         }
       },
-      err => {
-        if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login');
-      }
+      err => { if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login'); }
     );
   }
 }
