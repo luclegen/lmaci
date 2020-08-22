73be98916ca88dcc6b8e110382ca208ae20ec688
luclegen <luc.dev.sun@gmail.com>
2020-08-22 17:08:18 +0700

    Change Email Component: ngOnInit

--- client/src/app/components/body/auth/change-password/change-password.component.ts
+++ client/src/app/components/body/auth/change-password/change-password.component.ts
@@ -26,14 +26,7 @@ export class ChangePasswordComponent implements OnInit {
   }
   
   ngOnInit(): void {
-    this.authService.getInfo().subscribe(
-      res => {
-        if (!res['user'].activated) this.router.navigateByUrl('active');
-      },
-      err => {
-        if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login');
-      }
-    );
+    this.authService.getInfo().subscribe(res => { if (!res['user'].activated) this.router.navigateByUrl('active'); }, err => { if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login'); });
   }
 
   checkStrengthPassword() {
