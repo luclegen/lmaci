692a5c40e514373b83c6b3305bd6a35858546325
luclegen <luc.dev.sun@gmail.com>
2020-09-03 11:04:01 +0700

    Product List Component: constructor

--- client/src/app/components/body/product-list/product-list.component.ts
+++ client/src/app/components/body/product-list/product-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+import { HelperService } from 'src/app/services/helper.service';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-product-list',
@@ -10,12 +14,51 @@ export class ProductListComponent implements OnInit {
 
   type;
   name;
+  products;
 
-  constructor(private route: ActivatedRoute) { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+              private titleService: Title,
+              private productsService: ProductsService,
+              private helperService: HelperService) {
     const path = this.route.snapshot.paramMap.get('path').split('-');
     [ this.type, this.name ] = [ path[0], path.slice(1).join(' ') ];
+
+    let title = 'No title';
+
+    switch (this.name) {
+      case 'macbook':
+        title = 'Macbook';
+        break;
+        
+      case 'ipad':
+        title = 'iPad';
+        break;
+
+      case 'iphone':
+        title = 'iPhone';
+        break;
+        
+      case 'apple watch':
+        title = this.helperService.toName(this.name);
+        break;
+        
+      case 'hp':
+        title = 'HP ' + this.helperService.toName(this.type);
+        break;
+        
+      case 'oppo':
+        title = 'OPPO ' + this.helperService.toName(this.type);
+        break;
+    
+      default:
+        title = this.helperService.toName(this.name + ' ' + this.type);
+        break;
+    }
+
+    this.titleService.setTitle(title + ' | Lmaci ');
+  }
+
+  ngOnInit(): void {
     
   }
 
