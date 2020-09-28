e22c46706c18dbbbf8c78517ccf8db285ccfe682
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2020-09-28 09:31:22 +0700

    User Router: PUT updateUser

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const transfer = require('../helpers/transfer');
 
 const userCtrl = require('../controllers/user.controller');
 
 router.get('/:username', userCtrl.get);
-    
+router.put('/:username', userCtrl.updateUser);
+
 module.exports = router;
