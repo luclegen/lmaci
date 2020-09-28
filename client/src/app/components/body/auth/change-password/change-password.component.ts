18a6ccd410d16e94a66791ddea5d6bb7eadda0f1
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2020-09-28 09:54:40 +0700

    Change Password Component: onSubmit

--- client/src/app/components/body/auth/change-password/change-password.component.ts
+++ client/src/app/components/body/auth/change-password/change-password.component.ts
@@ -90,6 +90,6 @@ export class ChangePasswordComponent implements OnInit {
   onSubmit(form: NgForm) {
     this.serverErrorMessages = null;
     this.successMessages = null;
-    if (this.matchPassword()) this.authService.getInfo().subscribe(res => { if (res['user'].activated) this.authService.changePassword(this.authService.getId(), form.value).subscribe(res => this.successMessages = res['msg'], err => this.serverErrorMessages = err.error.msg); }, err => { if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login'); });
+    if (this.matchPassword()) this.authService.getInfo().subscribe(res => { if (res['user']) this.authService.changePassword(this.authService.getId(), form.value).subscribe(res => this.successMessages = res['msg'], err => this.serverErrorMessages = err.error.msg); }, err => { if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login'); });
   }
 }
