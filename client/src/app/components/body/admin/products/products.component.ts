677f07d211d65437d1448d9d9d03bcea4daafae8
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-05 17:06:32 +0700

    Products Component: onSubmit

--- client/src/app/components/body/admin/products/products.component.ts
+++ client/src/app/components/body/admin/products/products.component.ts
@@ -286,7 +286,6 @@ export class ProductsComponent implements OnInit {
                       quantity: { imported: 1 },
                       type: '',
                       colors: [],
-                      capacitys: [],
                       properties: [],
                       technicalDetails: [],
                     };
