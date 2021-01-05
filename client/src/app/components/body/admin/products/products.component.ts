020c63c59aa868baa70728b1108406ffbfdb6cd5
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-05 21:18:30 +0700

    Products Component: onSubmit

--- client/src/app/components/body/admin/products/products.component.ts
+++ client/src/app/components/body/admin/products/products.component.ts
@@ -302,7 +302,7 @@ export class ProductsComponent implements OnInit {
           }
         }
       },
-      err => { if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login'); }
+      err => { if (err.status == 440 && confirm('Login again?\nYour session has expired and must log in again.')) window.open('/login'); }
     );
   }
 
