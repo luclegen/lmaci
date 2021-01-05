10b5a05497f4d900dd4435ecc875f24f4a01ce60
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-05 20:49:40 +0700

    Draft

--- draft/new.ts
+++
@@ -1,6 +0,0 @@
-
-  resetColor() {
-    this.isChangeColor = this.preview.color.value != this.order.color.value;
-    if (this.isChangeColor) this.preview.color = this.order.color;
-    this.reloadSlideshow();
-  }
