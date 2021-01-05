7380fb7ab8ab565671bfbe15e0a5fbb6512abebe
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-05 21:12:54 +0700

    Admins Component: removeAsAdmin

--- client/src/app/components/body/admin/admins/admins.component.ts
+++ client/src/app/components/body/admin/admins/admins.component.ts
@@ -62,7 +62,7 @@ export class AdminsComponent implements OnInit {
           }
         }
       },
-      err => { if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login'); }
+      err => { if (err.status == 440 && confirm('Login again?\nYour session has expired and must log in again.')) window.open('/login'); }
     );
   }
 
