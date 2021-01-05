f4f92aa0e4f3636fe6a894332855f4628550138b
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-05 20:49:27 +0700

    Product Component: resetColor

--- client/src/app/components/body/product/product.component.ts
+++ client/src/app/components/body/product/product.component.ts
@@ -575,6 +575,7 @@ export class ProductComponent implements OnInit {
   }
 
   resetColor() {
+    this.isChangeColor = this.preview.color.value != this.order.color.value;
     if (this.isChangeColor) this.preview.color = this.order.color;
     this.reloadSlideshow();
   }
