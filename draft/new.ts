3a1af29f521e8fd15fcb95594a20ed9c6529401b
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-06 08:14:17 +0700

    Draft

--- draft/new.ts
+++
@@ -1,6 +0,0 @@
-
-  onSubmit(form: NgForm) {
-    this.serverErrorMessages = null;
-    this.successMessages = null;
-    if (this.matchPassword()) this.authService.getInfo().subscribe(res => { if (res['user']) this.authService.changePassword(this.authService.getId(), form.value).subscribe(res => this.successMessages = res['msg'], err => this.serverErrorMessages = err.error.msg); }, err => { if (err.status == 440 && confirm('Your session has expired and must log in again.\n\nDo you want to login again?')) window.open('/login'); });
-  }
