e8f81a63a52acd68b1280acb44a1e6ce1b995ee3
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-12 17:57:31 +0700

    Admin Component: ngOnInit

--- client/src/app/components/body/admin/admin.component.ts
+++ client/src/app/components/body/admin/admin.component.ts
@@ -20,10 +20,11 @@ export class AdminComponent implements OnInit {
 
   ngOnInit(): void {
     const vw = document.documentElement.clientWidth;
+    const vh = document.documentElement.clientHeight;
     const section = document.querySelector('section') as HTMLElement;
 
     section.style.fontSize = vw * 0.01625 + 'px';
-    this.helperService.setNavbar();
+    section.style.minHeight = vh - 60 + 'px';
 
     this.authService.getInfo().subscribe(res => { if (!res['user'].activated || res['user'].role == 'user') this.router.navigateByUrl(''); }, err => { if (err.status == 440 && confirm('Login again?\nYour session has expired and must log in again.')) window.open('/login'); });
   }
