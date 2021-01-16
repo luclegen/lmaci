99874830a57cd9e5f91d346b22ac9d3d9e51960c
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-16 20:58:19 +0700

    Products Component: onEditProduct

--- client/src/app/components/body/admin/products/products.component.ts
+++ client/src/app/components/body/admin/products/products.component.ts
@@ -575,9 +575,15 @@ export class ProductsComponent implements OnInit {
   }
 
   onEditProduct(p: Object) {
-    this.imageChangedEvent = this.croppedImage = '';
+    this.authService.getInfo().subscribe(res => { if (res['user'].role == 'root' || res['user'].role === 'admin') this.adminService.getProducts().subscribe(
+      res => {
+        const index = this.products.indexOf(p);
 
-    this.product = JSON.parse(JSON.stringify(p));
+        this.products = res['products'];
+        this.product = this.products[index];
+        this.imageChangedEvent = this.croppedImage = '';
+      }, err => alert(err.error.msg)); }, err => { if (err.status == 440 && confirm('Login again?\nYour session has expired and must log in again.')) window.open('/login'); 
+    });
   }
 
   //#endregion Edit
