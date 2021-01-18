f81124d37230a1196a424e3706f32795104db673
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-18 09:44:11 +0700

    Authentication

--- client/src/app/auth/auth.guard.ts
+++ client/src/app/auth/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate } from '@angular/router';
 
 import { AuthService } from '../services/auth.service';
 
@@ -10,9 +10,7 @@ export class AuthGuard implements CanActivate {
 
   constructor(private authService: AuthService) { }
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
+  canActivate(): boolean {
     return this.authService.isLoggedIn();
   }
   
