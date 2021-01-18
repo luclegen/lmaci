0cdac0197cf12980a63ffaa6c9f66909153fbd73
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-18 17:54:42 +0700

    Admin Guard

--- client/src/app/guards/admin/admin.guard.spec.ts
+++ client/src/app/guards/admin/admin.guard.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { DataGuard } from './data.guard';
+import { AdminGuard } from './admin.guard';
 
-describe('DataGuard', () => {
-  let guard: DataGuard;
+describe('AdminGuard', () => {
+  let guard: AdminGuard;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    guard = TestBed.inject(DataGuard);
+    guard = TestBed.inject(AdminGuard);
   });
 
   it('should be created', () => {
---
+++ client/src/app/guards/admin/admin.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanDeactivate, Router } from '@angular/router';
+
+import { AuthService } from 'src/app/services/auth.service';
+
+import { ProductsComponent } from 'src/app/components/body/admin/products/products.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate, CanDeactivate<ProductsComponent> {
+
+  constructor(private authService: AuthService) { }
+
+  canActivate(): boolean {
+    return this.authService.isLoggedIn() && this.authService.activated() && this.authService.isAdmin();
+  }
+
+  canDeactivate(productsComponent: ProductsComponent) {
+    return productsComponent.isInput ? confirm('Leave site?\nChanges you made may not be saved.') : true;
+  }
+
+}
