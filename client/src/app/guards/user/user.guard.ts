81582a7ea2a937888dfa9cac4a736d1ebf25cff5
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-18 17:55:05 +0700

    User Guard

---
+++ client/src/app/guards/user/user.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserGuard implements CanActivate {
+
+  constructor(private authService: AuthService) { }
+
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    return this.authService.isLoggedIn() && this.authService.activated() && this.authService.isAdmin() || this.authService.isLoggedIn() && this.authService.activated() && this.authService.getUsername() == route.paramMap.get('username');
+  }
+
+}
