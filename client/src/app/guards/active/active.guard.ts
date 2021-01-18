34d97bf2ef9f6537926a28a9414e8315fed683b1
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-18 21:51:47 +0700

    Active Guard

---
+++ client/src/app/guards/active/active.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+
+import { AuthService } from 'src/app/services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActiveGuard implements CanActivate {
+  
+  constructor(private authService: AuthService) { }
+
+  canActivate(): boolean {
+    return this.authService.isLoggedIn() && !this.authService.activated();
+  }
+
+}
