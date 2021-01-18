36a722e04b474a7e311f2888300ec77ffbec9e64
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-18 21:51:59 +0700

    User Guard

--- client/src/app/guards/user/user.guard.ts
+++ client/src/app/guards/user/user.guard.ts
@@ -11,7 +11,7 @@ export class UserGuard implements CanActivate {
   constructor(private authService: AuthService) { }
 
   canActivate(route: ActivatedRouteSnapshot): boolean {
-    return this.authService.isLoggedIn() && this.authService.activated() && this.authService.isAdmin() || this.authService.isLoggedIn() && this.authService.activated() && this.authService.getUsername() == route.paramMap.get('username');
+    return this.authService.isLoggedIn() && this.authService.activated() && this.authService.isAdmin() || this.authService.isLoggedIn() && this.authService.getUsername() == route.paramMap.get('username');
   }
 
 }
