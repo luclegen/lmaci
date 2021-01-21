bb408d3122f3c63765ee342bbcec78b30703d8c1
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-21 18:09:44 +0700

    Admin Guard

--- client/src/app/guards/admin/admin.guard.ts
+++ client/src/app/guards/admin/admin.guard.ts
@@ -17,7 +17,7 @@ export class AdminGuard implements CanActivate, CanDeactivate<ProductsComponent>
   }
 
   canDeactivate(productsComponent: ProductsComponent) {
-    return productsComponent.isInput ? confirm('Leave site?\nChanges you made may not be saved.') : true;
+    return productsComponent.canDeactive() ? true : confirm('Leave site?\nChanges you made may not be saved.');
   }
 
 }
