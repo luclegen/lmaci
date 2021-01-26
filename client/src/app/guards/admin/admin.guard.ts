aae6c1f81ff83f73be07d02f8e5f818660e86527
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-26 19:06:50 +0700

    Admin Guard

--- client/src/app/guards/admin/admin.guard.ts
+++ client/src/app/guards/admin/admin.guard.ts
@@ -4,20 +4,25 @@ import { CanActivate, CanDeactivate, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 import { ProductsComponent } from 'src/app/components/body/admin/products/products.component';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate, CanDeactivate<ProductsComponent> {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private adminService: AdminService) { }
 
   canActivate(): boolean {
     return this.authService.loggedIn() && this.authService.activated() && this.authService.isAdmin();
   }
 
   canDeactivate(productsComponent: ProductsComponent) {
-    return productsComponent.canDeactivate() ? true : confirm('Leave site?\nChanges you made may not be saved.');
+    if (productsComponent.canDeactivate()) return true;
+    else if (confirm('Leave site?\nChanges you made may not be saved.')) {
+      this.adminService.finishEdit(productsComponent.product);
+      return true;
+    } else return false;
   }
 
 }
