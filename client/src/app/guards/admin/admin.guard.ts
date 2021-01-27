1e04f3065cf1067cbc5af8b51d8383b6d7671fd1
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-01-27 15:45:17 +0700

    Revert "Admin Guard"
    This reverts commit aae6c1f81ff83f73be07d02f8e5f818660e86527.

--- client/src/app/guards/admin/admin.guard.ts
+++ client/src/app/guards/admin/admin.guard.ts
@@ -4,25 +4,20 @@ import { CanActivate, CanDeactivate, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 import { ProductsComponent } from 'src/app/components/body/admin/products/products.component';
-import { AdminService } from 'src/app/services/admin.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate, CanDeactivate<ProductsComponent> {
 
-  constructor(private authService: AuthService, private adminService: AdminService) { }
+  constructor(private authService: AuthService) { }
 
   canActivate(): boolean {
     return this.authService.loggedIn() && this.authService.activated() && this.authService.isAdmin();
   }
 
   canDeactivate(productsComponent: ProductsComponent) {
-    if (productsComponent.canDeactivate()) return true;
-    else if (confirm('Leave site?\nChanges you made may not be saved.')) {
-      this.adminService.finishEdit(productsComponent.product);
-      return true;
-    } else return false;
+    return productsComponent.canDeactivate() ? true : confirm('Leave site?\nChanges you made may not be saved.');
   }
 
 }
