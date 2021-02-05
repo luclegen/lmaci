3bbb81d84aef04b51fd98a88f3f7178c19bd80be
Luc Huynh Tan <luc.dev.sun@gmail.com>
2021-02-05 15:07:58 +0700

    Helpers: Generator

--- server/helpers/generator.js
+++ server/helpers/generator.js
@@ -1,5 +1,1 @@
-const Code = require('../models/code.model');
-
 module.exports.getCode = length => '0'.repeat(length).split("").map(() => Math.floor(Math.random() * 10)).join("");
-
-module.exports.deleteCode = async id => await Code.deleteOne({ _userId: id }, err => { if (err) console.log('ERROR: Clear codes: ' + JSON.stringify(err, undefined, 2)); });
