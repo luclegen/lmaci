5024ab2b55fd9433c16efbfb119e33c84ac41c05
Luc Huynh Tan <luc.dev.sun@gmail.com>
2021-02-05 15:07:24 +0700

    Code Model

--- server/models/code.model.js
+++ server/models/code.model.js
@@ -1,6 +1,7 @@
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
-let codeSchema = new mongoose.Schema({
+const codeSchema = new mongoose.Schema({
   _userId: {
     type: mongoose.Schema.Types.ObjectId,
     required: 'User Id can\'t be empty',
@@ -8,12 +9,29 @@ let codeSchema = new mongoose.Schema({
   },
   code: {
     type: String,
-    required: 'Code can\'t be empty',
+    required: 'Code can\'t be empty'
   },
 }, {
   timestamps: {
     createdAt: 'createdAt'
   }
 });
+
+//#region Events
+
+codeSchema.pre('save', async function (next) {
+  this.code = await bcrypt.hash(this.code, await bcrypt.genSalt(10));
+  next();
+});
+
+//#endregion Events
+
+//#region Methods
+
+codeSchema.methods.verified = function (code) {
+  return bcrypt.compareSync(code, this.code);
+}
+
+//#endregion Methods
 
 module.exports = mongoose.model('Code', codeSchema);
