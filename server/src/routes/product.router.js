4311ef27952a515c1733ebb5f0f8b49516ea3adc
Luc Huynh Tan <luc.dev.sun@gmail.com>
2021-02-07 18:22:02 +0700

    Change

--- server/src/routes/product.router.js
+++ server/src/routes/product.router.js
@@ -3,13 +3,13 @@ const productCtrl = require('../controllers/product.controller');
 const transfer = require('../helpers/transfer');
 
 router.get('/:id', productCtrl.get);
-router.put('/upload-slideshow/:id', transfer.upload('uploads/img/product', 'slideshow').array('files'), productCtrl.uploadSlideshow);
+router.put('/upload-slideshow/:id', transfer.upload(process.env.PRODUCT_IMG, 'slideshow').array('files'), productCtrl.uploadSlideshow);
 router.put('/post/:id', productCtrl.post);
-router.put('/review/:id', transfer.upload('uploads/img/product', 'review').array('files'), productCtrl.review);
+router.put('/review/:id', transfer.upload(process.env.PRODUCT_IMG, 'review').array('files'), productCtrl.review);
 router.put('/delete-review/:id', productCtrl.deleteReview);
-router.put('/comment/:id', transfer.upload('uploads/img/product', 'comment').array('files'), productCtrl.comment);
+router.put('/comment/:id', transfer.upload(process.env.PRODUCT_IMG, 'comment').array('files'), productCtrl.comment);
 router.put('/delete-comment/:id', productCtrl.deleteComment);
-router.put('/reply/:id', transfer.upload('uploads/img/product', 'answer', 'comment').array('files'), productCtrl.reply);
+router.put('/reply/:id', transfer.upload(process.env.PRODUCT_IMG, 'answer', 'comment').array('files'), productCtrl.reply);
 router.put('/delete-answer/:id', productCtrl.deleteAnswer);
 
 module.exports = router;
