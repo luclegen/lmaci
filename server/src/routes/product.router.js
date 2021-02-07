fae0a8f797f57df2e744fe79f8fa02e37f0b16e5
Luc Huynh Tan <luc.dev.sun@gmail.com>
2021-02-07 12:22:12 +0700

    Optimize product router

--- server/src/routes/product.router.js
+++ server/src/routes/product.router.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 
 const productCtrl = require('../controllers/product.controller');
 const transfer = require('../helpers/transfer');
