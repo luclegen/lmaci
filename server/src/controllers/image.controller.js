a8314bcc36265724eb4a0ee74bca4b7c498c4ad7
Luc Huynh Tan <luc.dev.sun@gmail.com>
2021-02-07 17:52:10 +0700

    Update get event

--- server/src/controllers/image.controller.js
+++ server/src/controllers/image.controller.js
@@ -3,23 +3,22 @@ const url = require('url');
 const transfer = require('../helpers/transfer');
 
 module.exports.get = (req, res) => {
-  const query = url.parse(req.url, true).query;
-  const pic = query.image;
-  const imageDir = './uploads/img/';
+  const pic = url.parse(req.url, true).query.image;
+  const imageDir = process.env.UPLOADS_IMG;
 
   if (typeof pic === 'undefined') {
     transfer.getImages(imageDir, function (err, files) {
-      res.writeHead(200, {'Content-type':'text/html'});
+      res.writeHead(200, { 'Content-type':'text/html' });
       res.end();
     });
   } else {
     fs.readFile(imageDir + pic, function (err, content) {
       if (err) {
-        res.writeHead(400, {'Content-type':'text/html'})
+        res.writeHead(400, { 'Content-type':'text/html' })
         console.log(err);
         res.end('No such image');    
       } else {
-        res.writeHead(200,{'Content-type':'image'});
+        res.writeHead(200,{ 'Content-type':'image' });
         res.end(content);
       }
     });
