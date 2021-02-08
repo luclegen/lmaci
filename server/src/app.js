313fab89d855dba11d43526f45a5ede5c1824d6d
Luc Huynh Tan <luc.dev.sun@gmail.com>
2021-02-08 13:24:34 +0700

    Update error handle

--- server/src/app.js
+++ server/src/app.js
@@ -22,7 +22,7 @@ app.use('/products', require('./routes/products.router'));
 app.use('/image', require('./routes/image.router'));
 
 // Error handle
-app.use((err, req, res, next) => res.status(442).send({ msg: err.name == 'ValidationError' ? Object.values(err.errors).map((e, i) => (i + 1) + '. ' + e).join(', ') + '!' : err.name }));
+app.use((err, req, res, next) => res.status(442).send(err.name == 'ValidationError' ? { msg: Object.values(err.errors).map((e, i) => (i + 1) + '. ' + e).join(', ') + '!' } : { error: err }));
 
 // Start server
 app.listen(process.env.PORT || 3000, () => console.log(`Server started at port: ${process.env.PORT || 3000}`));
