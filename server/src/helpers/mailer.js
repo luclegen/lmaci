5abe47407b4e5df60d360d5c54c8a79acdc3bfae
Luc Huynh Tan <huynhtanluc.auto@gmail.com>
2021-02-20 22:47:27 +0700

    Update Mailer helper

--- server/src/helpers/mailer.js
+++ server/src/helpers/mailer.js
@@ -12,7 +12,7 @@ const transport = nodemailer.createTransport({
 });
 
 module.exports.sendVerifyEmail = (clientEmail, title, code) => {
-  let mailOptions = {
+  const mailOptions = {
     from: {
       "address": process.env.SERVER_EMAIL,
       "name": process.env.SERVER_NAME
@@ -23,8 +23,5 @@ module.exports.sendVerifyEmail = (clientEmail, title, code) => {
     html: '<h1><img src="https://raw.githubusercontent.com/luclegen/lmaci/master/client/src/assets/img/logo/lmaci-logo-64.png" alt="Lmaci Logo"> Lmaci</h1><hr><h1>' + title + '</h1><p>Verification Code: ' + code + '</p>'
   }
 
-  transport.sendMail(mailOptions, (err, info) => {
-    if (err) console.log(err);
-    else console.log('Email sent: ' + info.response);
-  });
+  transport.sendMail(mailOptions, (err, info) => console.log(err ? err : 'Sent email: ' + info.response));
 }
